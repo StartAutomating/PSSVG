9880f34e53f706ad4dbcab052308497568289557
James Brundage <@github.com>
2022-09-07 23:01:45 -0700

    Adding Module (Fixes #1)

---
+++ PSSVG.psd1
@@ -0,0 +1,10 @@
+@{
+    ModuleVersion     = '0.1'
+    Guid              = '77696e6e-0252-43f2-b914-2dfa63953c60'
+    CompanyName       = 'Start-Automating'
+    Copyright         = '2022 Start-Automating'
+    Author            = 'James Brundage'
+    AliasesToExport   = '*'
+    RootModule        = 'PSSVG.psm1'
+    Description       = 'Script SVGs with PowerShell'
+}
---
+++ PSSVG.psm1
@@ -0,0 +1,18 @@
+﻿foreach ($file in Get-ChildItem -Filter *.ps1 -Path $PSScriptRoot) {            
+    if ($file.Name -match '^SVG\.')  {
+        . $file.FullName
+        $functionName = $file.Name -replace '\.ps1$'
+        $aliasName = $functionName -replace '^SVG\.'
+        if ($aliasName -eq 'svg') {
+            Set-Alias "=<svg>" "$functionName" 
+            Set-Alias "svg" "$functionName"
+        } else {
+            Set-Alias "=<svg.$aliasName>" "$functionName"            
+        }        
+    }
+    elseif ($file.Name -like '*-*.ps1') {
+        . $file.FullName
+    }
+}
+
+Export-ModuleMember -Alias * -Function *
