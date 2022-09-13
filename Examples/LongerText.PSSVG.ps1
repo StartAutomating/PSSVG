d07cf6594bf37d84c3f065e61d818b4e5834ddfe
James Brundage <@github.com>
2022-09-12 18:14:00 -0700

    Updating LongerText example (fixing Viewbox)

--- Examples/LongerText.PSSVG.ps1
+++ Examples/LongerText.PSSVG.ps1
@@ -3,14 +3,22 @@
 $longText = dir | Out-String
 $longTextLines = @($longText -split '(?>\r\n|\n)')
 $fontSize = 14
+$lineSpacing = 1.2
+
+$ln = 0
+$maxLineLength =0 
+$goldenRatio   = (1 + [Math]::Sqrt(5)) / 2
 
 =<svg> @(
     $ln = 0
     foreach ($line in $longTextLines) {
         $ln++
-        =<svg.text> -X 0 -Y ($ln * $fontSize) -Fontsize $fontSize $longTextLines[$ln] -Xmlspace preserve -Fontfamily monospace
+        if ($line.Length -gt $maxLineLength) {
+            $maxLineLength = $line.Length
+        }
+        =<svg.text> -X 0 -Y ($ln * $fontSize * $lineSpacing) -Fontsize $fontSize $longTextLines[$ln] -Xmlspace preserve -Fontfamily monospace -Fill '#4488ff'
     }
     
-) -OutputPath (Join-Path $PSScriptRoot .\LongerText.svg)
+) -ViewBox "0 0 $(($maxLineLength * $fontSize)/$goldenRatio) $($ln * $fontSize * 1.2)" -OutputPath (Join-Path $PSScriptRoot .\LongerText.svg)
 
 
