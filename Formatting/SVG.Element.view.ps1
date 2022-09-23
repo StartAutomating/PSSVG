639ddfb71fc98c37a36176042ec408ee1723086a
James Brundage <@github.com>
2022-09-22 17:59:14 -0700

    Adding Formatting for SVG.Element (Fixes #35)

---
+++ Formatting/SVG.Element.view.ps1
@@ -0,0 +1,10 @@
+﻿[OutputType('SVG.Element')]
+param()
+
+Write-FormatViewExpression -If {$script:TreeDepth = 0;$true} -ControlName SVGNodeControl -ScriptBlock {
+    @(foreach ($cn in $_.ChildNodes) {
+        if ($cn -is [xml.xmldeclaration]) { continue }
+        $cn
+    })
+} -Enumerate
+Write-FormatViewExpression -If {$ExecutionContext.SessionState.PSVariable.Remove('script:TreeDepth'); $false} -ScriptBlock { $null }
