f3f80a74756f635b478599e48212051d3b0fd358
StartAutomating <StartAutomating@github.com>
2022-10-22 05:30:40 +0000

    Renaming SVG.RegularPolygon -> SVG.ConvexPolygon

---
+++ Commands/Custom/SVG.ConvexPolygon.ps1
@@ -0,0 +1,98 @@
+function SVG.ConvexPolygon {
+<#
+    .SYNOPSIS
+        SVG Convex Polygon
+    .DESCRIPTION
+        Creates a Regular Convex Polygon of an number of sides.
+    .LINK
+        SVG.Path
+    
+#>
+    
+[CmdletBinding(PositionalBinding=$false)]
+    param(
+# The number of sides in the polygon
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('NumberOfSides','SC','Sides','NumSides','PC','D','PointCount')]
+    [ValidateRange(3,360)]
+    [int]
+    $SideCount,
+# The initial rotation of the polygon.
+    [Alias('Rotation')]
+    [double]
+    $Rotate = 0,
+# The center X coordinate for the polygon.
+    [Alias('CX')]
+    [double]
+    $CenterX = 1,
+# The center Y coordinate for the polygon.
+    [Alias('CY')]
+    [double]
+    $CenterY = 1,
+# The radius of the polygon.
+    [Alias('R')]
+    [double]
+    $Radius = 1
+    )
+dynamicParam {
+    $baseCommand = 
+        if (-not $script:SVGPath) {
+            $script:SVGPath = 
+                $executionContext.SessionState.InvokeCommand.GetCommand('SVG.Path','Function')
+            $script:SVGPath
+        } else {
+            $script:SVGPath
+        }
+    $IncludeParameter = @()
+    $ExcludeParameter = 'D'
+    $DynamicParameters = [Management.Automation.RuntimeDefinedParameterDictionary]::new()            
+    :nextInputParameter foreach ($paramName in ([Management.Automation.CommandMetaData]$baseCommand).Parameters.Keys) {
+        if ($ExcludeParameter) {
+            foreach ($exclude in $ExcludeParameter) {
+                if ($paramName -like $exclude) { continue nextInputParameter}
+            }
+        }
+        if ($IncludeParameter) {
+            $shouldInclude = 
+                foreach ($include in $IncludeParameter) {
+                    if ($paramName -like $include) { $true;break}
+                }
+            if (-not $shouldInclude) { continue nextInputParameter }
+        }
+        
+        $DynamicParameters.Add($paramName, [Management.Automation.RuntimeDefinedParameter]::new(
+            $baseCommand.Parameters[$paramName].Name,
+            $baseCommand.Parameters[$paramName].ParameterType,
+            $baseCommand.Parameters[$paramName].Attributes
+        ))
+    }
+    $DynamicParameters
+}
+    process {
+        # We can construct a regular polygon by creating N points along a unit circle
+        $anglePerPoint = 360 / $SideCount
+        $r = $Radius
+        $angle = $Rotate
+        $points = @(
+        foreach ($sideNumber in 1..$SideCount) {
+            $pointY = $centerY + $r * [math]::round([math]::cos($angle * [Math]::PI/180),15)
+            $pointX = $centerX + $r * [math]::round([math]::sin($angle * [Math]::PI/180),15)
+            if ($sideNumber -eq 1) {
+                "M $pointX $pointY"
+            } else {
+                "L $pointX $pointY"
+            }            
+            $angle += $anglePerPoint
+        }) -join ' '
+        $myParams = @{} + $PSBoundParameters
+        $myParams["D"] = $points
+        $myParams.Remove('SideCount')
+        $myParams.Remove('Rotate')
+        $myParams.Remove('Radius')
+        $myParams.Remove('CenterX')
+        $myParams.Remove('CenterY')
+              
+        svg.Path @myParams  
+    
+}
+}
