56575b7d5692bf5315c63163453e606e95f39bf3
StartAutomating <StartAutomating@github.com>
2023-01-09 22:19:29 +0000

    Updating Build (using Invoke-GitHubRestAPI)

--- Commands/Standard/SVG.feBlend.ps1
+++ Commands/Standard/SVG.feBlend.ps1
@@ -13,6 +13,7 @@ function SVG.feBlend {
 #>
 [Reflection.AssemblyMetadata('SVG.ElementName', 'feBlend')]
 [CmdletBinding(PositionalBinding=$false)]
+[OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feBlend element
 [Parameter(Position=0,ValueFromPipelineByPropertyName)]
