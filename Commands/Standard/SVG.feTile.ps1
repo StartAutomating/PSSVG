7a4651d3e4dba133c89a5e4cdf8e4d7c333d376e
StartAutomating <StartAutomating@github.com>
2023-01-09 23:58:24 +0000

    Updating build (improving inner help and adding README note.)

--- Commands/Standard/SVG.feTile.ps1
+++ Commands/Standard/SVG.feTile.ps1
@@ -1441,7 +1441,9 @@ process {
             $writeSvgSplat.OutputPath = $paramCopy['OutputPath']
         }
 
+        # If we provided any -Data attributes
         if ($data) {
+            # put it into the splat.
             $writeSvgSplat.Data = $data
         }
 
