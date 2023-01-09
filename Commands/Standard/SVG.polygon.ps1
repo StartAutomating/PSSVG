899c465ee265379be2e4d995aa5b0d2e9a197473
StartAutomating <StartAutomating@github.com>
2023-01-09 22:19:30 +0000

    Updating Build (using Invoke-GitHubRestAPI)

--- Commands/Standard/SVG.polygon.ps1
+++ Commands/Standard/SVG.polygon.ps1
@@ -105,25 +105,6 @@ function SVG.polygon {
     
         =<svg.use> -Href '#psChevron' -Fill '#4488ff'
     )
-.Example
-    =<svg> -viewBox 300, 100 -Content @(
-        =<svg.symbol> -Id psChevron -Content @(
-            =<svg.polygon> -Points (@(
-                "40,20"
-                "45,20"
-                "60,50"
-                "35,80"
-                "32.5,80"
-                "55,50"
-            ) -join ' ')
-        ) -ViewBox 100, 100
-        =<svg.use> -Href '#psChevron' -Fill '#4488ff' -X -7.5%
-        =<svg.text> @(
-            =<svg.tspan> -Content 'Start' -LetterSpacing .15em -AlignmentBaseline 'middle'
-            =<svg.tspan> -Content 'Automating' -LetterSpacing .2em -AlignmentBaseline 'middle' -Dx 0.5em
-        ) -FontFamily 'monospace' -AlignmentBaseline 'middle' -X 27.5% -Y 50% -Fill '#4488ff'
-        # =<svg.text> -Content 'Automating' -FontFamily 'monospace' -AlignmentBaseline 'middle' -X 45% -Y 55% -Fill '#4488ff' -LetterSpacing .1em
-    )
 .Link
     https://pssvg.start-automating.com/SVG.polygon
 .Link
@@ -133,6 +114,7 @@ function SVG.polygon {
 #>
 [Reflection.AssemblyMetadata('SVG.ElementName', 'polygon')]
 [CmdletBinding(PositionalBinding=$false)]
+[OutputType([Xml.XmlElement])]
 param(
 # The Contents of the polygon element
 [Parameter(Position=0,ValueFromPipelineByPropertyName)]
