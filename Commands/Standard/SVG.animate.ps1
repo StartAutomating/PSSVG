a8f4fc1d54837b3948d62c24e3c6a131de8e6013
StartAutomating <StartAutomating@github.com>
2023-01-09 22:26:51 +0000

    Updating Examples (Ordering Splatting)

--- Commands/Standard/SVG.animate.ps1
+++ Commands/Standard/SVG.animate.ps1
@@ -1403,10 +1403,10 @@ function SVG.animate {
     =<svg> @(
         foreach ($n in 1..10) {
             $n10 = $n * 10
-            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style @{
+            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style ([Ordered]@{
                 fill   = $colors[$n % $colors.Length]
                 stroke = $colors[($n + 1) % $colors.Length]
-            } @(
+            }) @(
                 =<svg.animate> -AttributeName rx -Values "0;50;0" -Dur "10s" -RepeatCount indefinite
                 =<svg.animate> -AttributeName x -Values "$($n10);$(200 - $n10/2);$($n10);" -Dur "10s" -RepeatCount indefinite
             )
@@ -1417,10 +1417,10 @@ function SVG.animate {
     =<svg> @(
         foreach ($n in 1..10) {
             $n10 = $n * 10
-            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style @{
+            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style ([Ordered]@{
                 fill   = $colors[$n % $colors.Length]
                 stroke = $colors[($n + 1) % $colors.Length]
-            } @(
+            }) @(
                 =<svg.animate> -AttributeName rx -Values "0;50;0" -Dur "10s" -RepeatCount indefinite
                 =<svg.animate> -AttributeName x -Values "$($n10);$(200 - $n10/2);$($n10);" -Dur "10s" -RepeatCount indefinite
             )
