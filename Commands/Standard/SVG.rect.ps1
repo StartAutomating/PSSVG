19caa5251be356e6bfbfbd7479316c527a99548b
StartAutomating <StartAutomating@github.com>
2023-01-09 22:26:52 +0000

    Updating Examples (Ordering Splatting)

--- Commands/Standard/SVG.rect.ps1
+++ Commands/Standard/SVG.rect.ps1
@@ -337,10 +337,10 @@ function SVG.rect {
     =<svg> @(
         foreach ($n in 1..10) {
             $n10 = $n * 10
-            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style @{
+            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style ([Ordered]@{
                 fill   = $colors[$n % $colors.Length]
                 stroke = $colors[($n + 1) % $colors.Length]
-            } @(
+            }) @(
                 =<svg.animate> -AttributeName rx -Values "0;50;0" -Dur "10s" -RepeatCount indefinite
                 =<svg.animate> -AttributeName x -Values "$($n10);$(200 - $n10/2);$($n10);" -Dur "10s" -RepeatCount indefinite
             )
@@ -351,10 +351,10 @@ function SVG.rect {
     =<svg> @(
         foreach ($n in 1..10) {
             $n10 = $n * 10
-            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style @{
+            =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style ([Ordered]@{
                 fill   = $colors[$n % $colors.Length]
                 stroke = $colors[($n + 1) % $colors.Length]
-            }
+            })
         }
     )
 .Example
