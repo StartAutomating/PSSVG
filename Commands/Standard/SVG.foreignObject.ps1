347ecb0ed7e52f478d5db93a598def703a8e82a7
StartAutomating <StartAutomating@github.com>
2023-01-09 23:58:24 +0000

    Updating build (improving inner help and adding README note.)

--- Commands/Standard/SVG.foreignObject.ps1
+++ Commands/Standard/SVG.foreignObject.ps1
@@ -1548,7 +1548,9 @@ process {
             $writeSvgSplat.OutputPath = $paramCopy['OutputPath']
         }
 
+        # If we provided any -Data attributes
         if ($data) {
+            # put it into the splat.
             $writeSvgSplat.Data = $data
         }
 
