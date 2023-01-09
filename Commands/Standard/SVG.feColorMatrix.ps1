11b395f290304912bc71451533b0ab4ea28e6c62
StartAutomating <StartAutomating@github.com>
2023-01-09 23:58:23 +0000

    Updating build (improving inner help and adding README note.)

--- Commands/Standard/SVG.feColorMatrix.ps1
+++ Commands/Standard/SVG.feColorMatrix.ps1
@@ -1520,7 +1520,9 @@ process {
             $writeSvgSplat.OutputPath = $paramCopy['OutputPath']
         }
 
+        # If we provided any -Data attributes
         if ($data) {
+            # put it into the splat.
             $writeSvgSplat.Data = $data
         }
 
