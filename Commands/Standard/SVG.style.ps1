ad37ccb004e89f4f5ec59370bff165feb556bf98
StartAutomating <StartAutomating@github.com>
2023-01-09 23:58:25 +0000

    Updating build (improving inner help and adding README note.)

--- Commands/Standard/SVG.style.ps1
+++ Commands/Standard/SVG.style.ps1
@@ -197,7 +197,9 @@ process {
             $writeSvgSplat.OutputPath = $paramCopy['OutputPath']
         }
 
+        # If we provided any -Data attributes
         if ($data) {
+            # put it into the splat.
             $writeSvgSplat.Data = $data
         }
 
