cec23ee6dd86c7272765f9bd435f76631413b68d
James Brundage <@github.com>
2023-01-09 14:21:35 -0800

    Updating Examples (Ordering Splatting)

--- Examples/ManyRectangles.PSSVG.ps1
+++ Examples/ManyRectangles.PSSVG.ps1
@@ -4,10 +4,10 @@ $colors = @('red','green','blue')
 =<svg> @(
     foreach ($n in 1..10) {
         $n10 = $n * 10        
-        =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style @{
+        =<svg.rect> -X $n10 -Y $n10 -Width $n10 -Height $n10 -Style ([Ordered]@{
             fill   = $colors[$n % $colors.Length]
             stroke = $colors[($n + 1) % $colors.Length]
-        }
+        })
     }
 ) -OutputPath (Join-Path $PSScriptRoot ManyRectangles.svg)
 
