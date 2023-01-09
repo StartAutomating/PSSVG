44b99e6c9780188a858e29b69e28b8b5ed5c89d8
StartAutomating <StartAutomating@github.com>
2023-01-09 23:58:25 +0000

    Updating build (improving inner help and adding README note.)

--- Commands/Standard/SVG.tspan.ps1
+++ Commands/Standard/SVG.tspan.ps1
@@ -1614,7 +1614,9 @@ process {
             $writeSvgSplat.OutputPath = $paramCopy['OutputPath']
         }
 
+        # If we provided any -Data attributes
         if ($data) {
+            # put it into the splat.
             $writeSvgSplat.Data = $data
         }
 
