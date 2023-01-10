0bc43a6711a5b907cb3c9509deec5df379a2fed2
StartAutomating <StartAutomating@github.com>
2023-01-10 04:26:33 +0000

    Updating Clouds Example (avoiding multiline comments)

--- Commands/Standard/SVG.tspan.ps1
+++ Commands/Standard/SVG.tspan.ps1
@@ -36,6 +36,66 @@ function SVG.tspan {
         }
         )
     )
+.Example
+    =<svg> @(
+        =<svg.ConvexPolygon> -SideCount 8 -Rotate (360/16) -Fill '#dd0000' -Stroke white -CenterX 100 -CenterY 100 -Radius 100
+    
+        =<svg.text> -X 50% -Y 50% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 64 -FontFamily sans-serif -Fill white @(
+            =<svg.tspan> -Content "STOP" -Id stop
+            =<svg.animate> -Values '64;66;64' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    
+        =<svg.text> -X 50% -Y 65% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 12 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "USING" -Id using
+            =<svg.animate> -Values '12;13;12' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+    
+        )
+    
+        =<svg.text> -X 50% -Y 80% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 32 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "GIFS" -Id gif
+            =<svg.animate> -Values '28;30;28' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    ) -ViewBox 200,200
+.Example
+    =<svg> @(
+        =<svg.ConvexPolygon> -SideCount 8 -Rotate (360/16) -Fill '#dd0000' -Stroke white -CenterX 100 -CenterY 100 -Radius 100
+    
+        =<svg.text> -X 50% -Y 50% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 64 -FontFamily sans-serif -Fill white @(
+            =<svg.tspan> -Content "STOP" -Id stop
+            =<svg.animate> -Values '64;66;64' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    
+        =<svg.text> -X 50% -Y 65% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 12 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "USING" -Id using
+            =<svg.animate> -Values '12;13;12' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+    
+        )
+    
+        =<svg.text> -X 50% -Y 80% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 32 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "GIFS" -Id gif
+            =<svg.animate> -Values '28;30;28' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    ) -ViewBox 200,200
+.Example
+    =<svg> @(
+        =<svg.ConvexPolygon> -SideCount 8 -Rotate (360/16) -Fill '#dd0000' -Stroke white -CenterX 100 -CenterY 100 -Radius 100
+    
+        =<svg.text> -X 50% -Y 50% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 64 -FontFamily sans-serif -Fill white @(
+            =<svg.tspan> -Content "STOP" -Id stop
+            =<svg.animate> -Values '64;66;64' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    
+        =<svg.text> -X 50% -Y 65% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 12 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "USING" -Id using
+            =<svg.animate> -Values '12;13;12' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+    
+        )
+    
+        =<svg.text> -X 50% -Y 80% -DominantBaseline 'middle' -TextAnchor 'middle' -FontSize 32 -FontFamily sans-serif -Fill white -Content @(
+            =<svg.tspan> -Content "GIFS" -Id gif
+            =<svg.animate> -Values '28;30;28' -Dur 5s -AttributeName font-size -RepeatDur 'indefinite'
+        )
+    ) -ViewBox 200,200
 .Link
     https://pssvg.start-automating.com/SVG.tspan
 .Link
@@ -45,7 +105,6 @@ function SVG.tspan {
 #>
 [Reflection.AssemblyMetadata('SVG.ElementName', 'tspan')]
 [CmdletBinding(PositionalBinding=$false)]
-[OutputType([Xml.XmlElement])]
 param(
 # The Contents of the tspan element
 [Reflection.AssemblyMetaData('SVG.IsCData', $True)]
