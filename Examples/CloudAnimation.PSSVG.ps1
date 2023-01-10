adc270f89c376025f1ef8d5c315678c8e8b66ec4
James Brundage <@github.com>
2023-01-09 18:54:23 -0800

    Updating Cloud Examples

--- Examples/CloudAnimation.PSSVG.ps1
+++ Examples/CloudAnimation.PSSVG.ps1
@@ -4,13 +4,13 @@ $AnimationTimeframe = [Ordered]@{
     Dur = '2s'
     RepeatCount = 'indefinite'
 }
-=<SVG> -viewBox '0 0 200 200' -Content @(
+=<SVG> -viewBox 1920,1080 -Content @(
     =<SVG.filter> -id 'noise1' -x '0' -y '0' -width '100%' -height '100%' -Content @(
         =<SVG.feTurbulence> -baseFrequency '0.025' @(
             =<svg.animate> -AttributeName numOctaves -Values '1;6;1' @AnimationTimeframe
-            =<svg.animate> -AttributeName seed -Values '0;2;0' @AnimationTimeframe
+            =<svg.animate> -AttributeName seed -Values '0;5;0' @AnimationTimeframe
         ) -NumOctaves 4 -Type fractalNoise
-        =<svg.feBlend> -In 'SourceGraphic' -Mode color
+        =<svg.feBlend> -In 'SourceGraphic' -Mode color-burn
     )
-    =<SVG.rect> -x '0' -y '0' -width '200' -height '200' -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .5 # -Clip   
+    =<SVG.rect> -x '0' -y '0' -width 100% -height 100% -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .2
 ) -OutputPath (Join-Path $psScriptRoot "CloudAnimation.svg")
--- Examples/Clouds.PSSVG.ps1
+++ Examples/Clouds.PSSVG.ps1
@@ -7,10 +7,10 @@
     Generates a cloud effect using fractal noise and blending modes. 
 #>
 
-=<SVG> -viewBox 200, 200 -Content @(
+=<SVG> -viewBox 1920, 1080 -Content @(
     =<SVG.filter> -id 'noise1' -x '0' -y '0' -width '100%' -height '100%' -Content @(
         =<SVG.feTurbulence> -baseFrequency '0.025' -Type 'fractalNoise' -NumOctaves 4
         =<svg.feBlend> -In 'SourceGraphic' -Mode color-burn
     )
-    =<SVG.rect> -x '0' -y '0' -width '200' -height '200' -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .5
-) -OutputPath (Join-Path $psScriptRoot "Clouds.svg")
+    =<SVG.rect> -x '0' -y '0' -width 100% -height 100% -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .2
+) -OutputPath (Join-Path $psScriptRoot "Clouds.svg") -Width '100%' -Height '100%'
