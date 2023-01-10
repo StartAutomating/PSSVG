a4757986640b3a7fb3e8839282bb92f38b1a2b5c
StartAutomating <StartAutomating@github.com>
2023-01-10 04:34:15 +0000

    Updating Build (adding [OutputType()])

--- Commands/Standard/SVG.animateTransform.ps1
+++ Commands/Standard/SVG.animateTransform.ps1
@@ -142,6 +142,7 @@ function SVG.animateTransform {
 #>
 [Reflection.AssemblyMetadata('SVG.ElementName', 'animateTransform')]
 [CmdletBinding(PositionalBinding=$false)]
+[OutputType([Xml.XmlElement])]
 param(
 # The Contents of the animateTransform element
 [Parameter(Position=0,ValueFromPipelineByPropertyName)]
