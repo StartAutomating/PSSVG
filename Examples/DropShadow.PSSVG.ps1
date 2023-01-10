41b2e12d3ef8c3e98716f5f04f551da1ebbdaeec
James Brundage <@github.com>
2023-01-09 20:22:07 -0800

    Updating Drop Shadow Example (avoiding heredocs)

--- Examples/DropShadow.PSSVG.ps1
+++ Examples/DropShadow.PSSVG.ps1
@@ -3,8 +3,8 @@
 =<svg> @(
     =<svg.DropShadow> -DistanceY .75
         
-    =<svg.text> @"
+    =<svg.text> "
 Dropping Shadows
-"@ -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
+" -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
     
 ) -ViewBox 0,0,300,100 -OutputPath (Join-Path $PSScriptRoot .\DropShadow.svg)
