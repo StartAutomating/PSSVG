712548627b8cd71649b9d6682ac4f94f519193a5
James Brundage <@github.com>
2023-01-09 20:14:56 -0800

    Updating Embossed Example (avoiding heredocs)

--- Examples/Embossed.PSSVG.ps1
+++ Examples/Embossed.PSSVG.ps1
@@ -2,18 +2,18 @@
 
 =<svg> @(
     =<svg.filter> -id embossed @(
-        =<svg.feConvolveMatrix> -KernelMatrix @'
+        =<svg.feConvolveMatrix> -KernelMatrix '
         5 0 0
         0 0 0
         0 0 -5       
-'@
+'
         =<svg.feMerge> @(
             =<svg.feMergeNode>
             =<svg.feMergeNode> -In 'SourceGraphic'
         )          
     )
         
-    =<svg.text> @"
+    =<svg.text> "
 Embossed
-"@ -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'    
+" -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'  
 ) -ViewBox 0,0,300,100 -OutputPath (Join-Path $PSScriptRoot .\Embossed.svg) 
