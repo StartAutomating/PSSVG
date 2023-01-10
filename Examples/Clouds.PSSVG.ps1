eb95225cd5a268b25629f32f2ec1e53655e4738d
James Brundage <@github.com>
2023-01-09 20:23:56 -0800

    Updating Clouds Example (avoiding multiline comments)

--- Examples/Clouds.PSSVG.ps1
+++ Examples/Clouds.PSSVG.ps1
@@ -1,15 +1,14 @@
 #requires -Module PSSVG
 
-<#
-.SYNOPSIS
-    Generates clouds using SVG
-.DESCRIPTION
-    Generates a cloud effect using fractal noise and blending modes. 
-#>
+#.SYNOPSIS
+#    Generates clouds using SVG
+#.DESCRIPTION
+#    Generates a cloud effect using fractal noise and blending modes. 
 
 =<SVG> -viewBox 1920, 1080 -Content @(
     =<SVG.filter> -id 'noise1' -x '0' -y '0' -width '100%' -height '100%' -Content @(
         =<SVG.feTurbulence> -baseFrequency '0.025' -Type 'fractalNoise' -NumOctaves 4
+        =<SVG.feGaussianBlur> -stdDeviation 0.9
         =<svg.feBlend> -In 'SourceGraphic' -Mode color-burn
     )
     =<SVG.rect> -x '0' -y '0' -width 100% -height 100% -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .2
