301481602087205dfc3a0c7134e53633be96b687
StartAutomating <StartAutomating@github.com>
2023-01-10 00:15:31 +0000

    Updating AnimatedShapes Example ([ordered] splatting)

--- Commands/Standard/SVG.line.ps1
+++ Commands/Standard/SVG.line.ps1
@@ -176,7 +176,7 @@ $Tabindex,
 # You can use this attribute with any SVG element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','xml:base')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', '<iri>')]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
 $XmlBase,
@@ -189,7 +189,7 @@ $XmlBase,
 # You can use this attribute with any SVG element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','xml:lang')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', '<language-tag>')]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
 $XmlLang,
@@ -202,7 +202,7 @@ $XmlLang,
 # You can use this attribute with any SVG element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','xml:space')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'default | preserve')]
 [ArgumentCompleter({
     param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
@@ -254,7 +254,7 @@ $Style,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','requiredFeatures')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', '<list-of-features>')]
 [Reflection.AssemblyMetaData('SVG.Default value', 'true if not defined, false if null or empty string as value')]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
@@ -321,7 +321,7 @@ $BaselineShift,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','clip')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'auto | rect()')]
 [ArgumentCompleter({
     param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
@@ -488,7 +488,7 @@ $ColorInterpolationFilters,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','color-profile')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'auto | sRGB | <name> | <iri>')]
 [ArgumentCompleter({
     param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
@@ -619,7 +619,7 @@ $DominantBaseline,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','enable-background')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'accumulate | new [ <x><y><width><height> ]?')]
 [ArgumentCompleter({
     param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
@@ -873,7 +873,7 @@ $FontWeight,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','glyph-orientation-horizontal')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', '<angle>')]
 [Reflection.AssemblyMetaData('SVG.Default value', '0deg')]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
@@ -889,7 +889,7 @@ $GlyphOrientationHorizontal,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','glyph-orientation-vertical')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'auto | <angle>')]
 [ArgumentCompleter({
     param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
@@ -936,7 +936,7 @@ $ImageRendering,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','kerning')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
+[Obsolete()]
 [Reflection.AssemblyMetaData('SVG.Value', 'auto | <length>')]
 [ValidatePattern('(?>|\d+)')]
 [ArgumentCompleter({
