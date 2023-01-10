e7cff93b02af394f8c002b949c806903d59bd7c7
StartAutomating <StartAutomating@github.com>
2023-01-10 04:26:31 +0000

    Updating Clouds Example (avoiding multiline comments)

--- Commands/Standard/SVG.foreignObject.ps1
+++ Commands/Standard/SVG.foreignObject.ps1
@@ -13,7 +13,6 @@ function SVG.foreignObject {
 #>
 [Reflection.AssemblyMetadata('SVG.ElementName', 'foreignObject')]
 [CmdletBinding(PositionalBinding=$false)]
-[OutputType([Xml.XmlElement])]
 param(
 # The Contents of the foreignObject element
 [Parameter(Position=0,ValueFromPipelineByPropertyName)]
