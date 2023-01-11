9f97c361935fde2579deed6b3a06e4022188ab65
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:01 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.set.ps1
+++ Commands/Standard/SVG.set.ps1
@@ -20,7 +20,7 @@ function SVG.set {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the set element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
