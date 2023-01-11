080539fbb3960b389184303e2f6163f9e9273239
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.animate.ps1
+++ Commands/Standard/SVG.animate.ps1
@@ -1704,7 +1704,7 @@ function SVG.animate {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the animate element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
