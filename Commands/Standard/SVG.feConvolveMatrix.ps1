061726bf7d45b902fe97638e7d550b985530e55e
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feConvolveMatrix.ps1
+++ Commands/Standard/SVG.feConvolveMatrix.ps1
@@ -74,7 +74,7 @@ function SVG.feConvolveMatrix {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feConvolveMatrix element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
