289a6673788cc63c081b386a55305c5e2afca259
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.marker.ps1
+++ Commands/Standard/SVG.marker.ps1
@@ -32,7 +32,7 @@ function SVG.marker {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the marker element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
