eade31471c3af990a14fb6eb9b7b789f4f74a162
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.desc.ps1
+++ Commands/Standard/SVG.desc.ps1
@@ -20,7 +20,7 @@ function SVG.desc {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the desc element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
