de623a32b63767b1f19bcb728e3ca6cf34f8e1a5
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feSpotLight.ps1
+++ Commands/Standard/SVG.feSpotLight.ps1
@@ -17,7 +17,7 @@ function SVG.feSpotLight {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feSpotLight element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
