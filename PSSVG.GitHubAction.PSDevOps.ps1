362ccfce4a2deade5e20b986c54a46ca5b7e8394
James Brundage <@github.com>
2023-01-10 17:28:39 -0800

    Updating Action (Removing Output) (Fixes #94)

--- PSSVG.GitHubAction.PSDevOps.ps1
+++ PSSVG.GitHubAction.PSDevOps.ps1
@@ -1,4 +1,6 @@
 ﻿#requires -Module PSDevOps
 #requires -Module PSSVG
 Import-BuildStep -ModuleName PSSVG
-New-GitHubAction -Name "MakePSSVG" -Description 'Build SVGs in your Workflow (with PowerShell)' -Action PSSVG -Icon aperture -OutputPath .\action.yml 
+New-GitHubAction -Name "MakePSSVG" -Description @'
+Build SVGs in your Workflow (with PowerShell)
+'@ -Action PSSVG -Icon aperture -OutputPath .\action.yml 
