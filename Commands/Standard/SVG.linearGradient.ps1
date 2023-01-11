76cdc06215e9bd044fccfd155fc37c079ab2f0f4
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.linearGradient.ps1
+++ Commands/Standard/SVG.linearGradient.ps1
@@ -66,7 +66,7 @@ function SVG.linearGradient {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the linearGradient element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
