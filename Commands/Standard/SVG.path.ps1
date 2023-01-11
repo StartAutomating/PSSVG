775f8370867addd5bec2f72714fd44fd1df0da5b
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:01 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.path.ps1
+++ Commands/Standard/SVG.path.ps1
@@ -28,7 +28,7 @@ function SVG.path {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the path element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
