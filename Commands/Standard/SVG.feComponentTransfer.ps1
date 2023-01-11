9463f88c9d3afdd20dd08378964b0442b821a31a
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feComponentTransfer.ps1
+++ Commands/Standard/SVG.feComponentTransfer.ps1
@@ -18,7 +18,7 @@ function SVG.feComponentTransfer {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feComponentTransfer element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
