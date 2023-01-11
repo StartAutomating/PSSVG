4bff4807f7ca229744177ad551b5c82207713cce
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feColorMatrix.ps1
+++ Commands/Standard/SVG.feColorMatrix.ps1
@@ -66,7 +66,7 @@ function SVG.feColorMatrix {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feColorMatrix element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
