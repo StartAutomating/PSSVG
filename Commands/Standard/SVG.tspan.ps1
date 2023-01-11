d78190b9e81218e5cf7a57b1cd307d22f807951c
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:01 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.tspan.ps1
+++ Commands/Standard/SVG.tspan.ps1
@@ -109,7 +109,7 @@ function SVG.tspan {
 param(
 # The Contents of the tspan element
 [Reflection.AssemblyMetaData('SVG.IsCData', $True)]
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
