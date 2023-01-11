5371bcf51fd07eb28765c18a71b7538b3da9f75f
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.filter.ps1
+++ Commands/Standard/SVG.filter.ps1
@@ -87,7 +87,7 @@ function SVG.filter {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the filter element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
