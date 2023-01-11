587b7fcb25e2dbfd41f3fef7d235db475a7d7fd3
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.mask.ps1
+++ Commands/Standard/SVG.mask.ps1
@@ -35,7 +35,7 @@ function SVG.mask {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the mask element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
