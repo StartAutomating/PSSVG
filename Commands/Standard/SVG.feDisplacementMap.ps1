9baa020858e65edc49ff891e1461d1d4e59f34ae
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feDisplacementMap.ps1
+++ Commands/Standard/SVG.feDisplacementMap.ps1
@@ -22,7 +22,7 @@ function SVG.feDisplacementMap {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feDisplacementMap element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
