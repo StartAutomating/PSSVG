bf4cbb9364bc68fce4ba08c6563c9ef4c2969857
StartAutomating <StartAutomating@github.com>
2023-01-11 01:59:59 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.clipPath.ps1
+++ Commands/Standard/SVG.clipPath.ps1
@@ -18,7 +18,7 @@ function SVG.clipPath {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the clipPath element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
