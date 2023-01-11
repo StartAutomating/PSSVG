419460b1658f34004dec1417d9a727c4b7e62aee
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:00 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.feFuncB.ps1
+++ Commands/Standard/SVG.feFuncB.ps1
@@ -16,7 +16,7 @@ function SVG.feFuncB {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the feFuncB element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
