973e1977afd0d5c7125d8194e78d683448d9ae8b
StartAutomating <StartAutomating@github.com>
2023-01-11 02:00:01 +0000

    SVG.*: Allowing -Content from the pipeline

--- Commands/Standard/SVG.stop.ps1
+++ Commands/Standard/SVG.stop.ps1
@@ -217,7 +217,7 @@ function SVG.stop {
 [OutputType([Xml.XmlElement])]
 param(
 # The Contents of the stop element
-[Parameter(Position=0,ValueFromPipelineByPropertyName)]
+[Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
 # A dictionary containing data.  This data will be embedded in data- attributes.
