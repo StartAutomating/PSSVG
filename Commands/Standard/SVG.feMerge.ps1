427becdb0fb278aa70e3d942fbb740c9dbae285a
StartAutomating <StartAutomating@github.com>
2023-01-12 01:33:05 +0000

    Regenerating Examples (Fixes #95)

--- Commands/Standard/SVG.feMerge.ps1
+++ Commands/Standard/SVG.feMerge.ps1
@@ -5,36 +5,104 @@ function SVG.feMerge {
 .Description
     The **`<feMerge>`** SVG element allows filter effects to be applied concurrently instead of sequentially. This is achieved by other filters storing their output via the `result` attribute and then accessing it in a `feMergeNode` child.
 .Example
-    =<svg> @(
-        =<svg.filter> -id embossed @(
-            =<svg.feConvolveMatrix> -KernelMatrix '
+    svg @(
+        svg.filter -id embossed @(
+            svg.feConvolveMatrix -KernelMatrix '
             5 0 0
             0 0 0
             0 0 -5
     '
-            =<svg.feMerge> @(
-                =<svg.feMergeNode>
-                =<svg.feMergeNode> -In 'SourceGraphic'
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
             )
         )
     
-        =<svg.text> "
+        svg.text "
     Embossed
     " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
     ) -ViewBox 0,0,300,100
 .Example
-    =<svg> @(
-        =<svg.filter> -id dropShadow @(
-            =<svg.feDropShadow> -dx 0.5 -dy 0.75 -StdDeviation 0 @(
-                =<svg.animate> -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
-            )
-            =<svg.feMerge> @(
-                =<svg.feMergeNode>
-                =<svg.feMergeNode> -In 'SourceGraphic'
+    svg @(
+        svg.filter -id embossed @(
+            svg.feConvolveMatrix -KernelMatrix '
+            5 0 0
+            0 0 0
+            0 0 -5
+    '
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
             )
         )
     
-        =<svg.text> "
+        svg.text "
+    Embossed
+    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
+    ) -ViewBox 0,0,300,100
+.Example
+    svg @(
+        svg.filter -id embossed @(
+            svg.feConvolveMatrix -KernelMatrix '
+            5 0 0
+            0 0 0
+            0 0 -5
+    '
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
+            )
+        )
+    
+        svg.text "
+    Embossed
+    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
+    ) -ViewBox 0,0,300,100
+.Example
+    svg @(
+        svg.filter -id dropShadow @(
+            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
+                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
+            )
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
+            )
+        )
+    
+        svg.text "
+    Moving Shadows
+    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
+    ) -ViewBox 0,0,300,100
+.Example
+    svg @(
+        svg.filter -id dropShadow @(
+            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
+                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
+            )
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
+            )
+        )
+    
+        svg.text "
+    Moving Shadows
+    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
+    ) -ViewBox 0,0,300,100
+.Example
+    svg @(
+        svg.filter -id dropShadow @(
+            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
+                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
+            )
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
+            )
+        )
+    
+        svg.text "
     Moving Shadows
     " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
     ) -ViewBox 0,0,300,100
