45532c2efae9190f6d0096f54d0399447709fa36
StartAutomating <StartAutomating@github.com>
2023-01-12 01:33:04 +0000

    Regenerating Examples (Fixes #95)

--- Commands/Standard/SVG.animateMotion.ps1
+++ Commands/Standard/SVG.animateMotion.ps1
@@ -8,26 +8,26 @@ function SVG.animateMotion {
     > **Note:** To reuse an existing path, it will be necessary to use an `mpath` element inside the `<animateMotion>` element instead of the `path` attribute.
 .Example
     $path = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
-    =<svg> -viewBox "0 0 200 100" @(
-        =<svg.path> -d $path -Fill none -Stroke lightgrey
-        =<svg.circle> -r 5 -Fill red (
-            =<svg.animateMotion> -Dur 10s -RepeatCount 'indefinite' -Path $path
+    SVG -viewBox "0 0 200 100" @(
+        SVG.path -d $path -Fill none -Stroke lightgrey
+        SVG.circle -r 5 -Fill red (
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
         )
-        =<svg.rect> -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
-            =<svg.animateMotion> -Dur 10s -RepeatCount 'indefinite' -Path $path
-            =<svg.animateTransform> -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
+        SVG.rect -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateTransform -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
         )
     )
 .Example
     $path = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
-    =<svg> -viewBox "0 0 200 100" @(
-        =<svg.path> -d $path -Fill none -Stroke lightgrey
-        =<svg.circle> -r 5 -Fill red (
-            =<svg.animateMotion> -Dur 10s -RepeatCount 'indefinite' -Path $path
+    SVG -viewBox "0 0 200 100" @(
+        SVG.path -d $path -Fill none -Stroke lightgrey
+        SVG.circle -r 5 -Fill red (
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
         )
-        =<svg.rect> -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
-            =<svg.animateMotion> -Dur 10s -RepeatCount 'indefinite' -Path $path
-            =<svg.animateTransform> -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
+        SVG.rect -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateTransform -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
         )
     )
 .Link
