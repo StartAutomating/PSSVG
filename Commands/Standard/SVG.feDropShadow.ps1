4ae9ddc2aba77170633240da817ed4965f13b64c
StartAutomating <StartAutomating@github.com>
2023-01-12 01:33:05 +0000

    Regenerating Examples (Fixes #95)

--- Commands/Standard/SVG.feDropShadow.ps1
+++ Commands/Standard/SVG.feDropShadow.ps1
@@ -7,18 +7,18 @@ function SVG.feDropShadow {
     
     > **Note:** The drop shadow color and opacity can be changed by using the `flood-color` and `flood-opacity` presentation attributes.
 .Example
-    =<svg> @(
-        =<svg.filter> -id dropShadow @(
-            =<svg.feDropShadow> -dx 0.5 -dy 0.75 -StdDeviation 0 @(
-                =<svg.animate> -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
+    svg @(
+        svg.filter -id dropShadow @(
+            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
+                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
             )
-            =<svg.feMerge> @(
-                =<svg.feMergeNode>
-                =<svg.feMergeNode> -In 'SourceGraphic'
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
             )
         )
     
-        =<svg.text> "
+        svg.text "
     Moving Shadows
     " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
     ) -ViewBox 0,0,300,100
