da854549d6a7b805a9188cb248fde568a8d76e37
StartAutomating <StartAutomating@github.com>
2023-01-12 01:33:05 +0000

    Regenerating Examples (Fixes #95)

--- Commands/Standard/SVG.feConvolveMatrix.ps1
+++ Commands/Standard/SVG.feConvolveMatrix.ps1
@@ -45,20 +45,20 @@ function SVG.feConvolveMatrix {
      3*200 + 2*220 + 1*240) / (9+8+7+6+5+4+3+2+1)
     ```
 .Example
-    =<svg> @(
-        =<svg.filter> -id embossed @(
-            =<svg.feConvolveMatrix> -KernelMatrix '
+    svg @(
+        svg.filter -id embossed @(
+            svg.feConvolveMatrix -KernelMatrix '
             5 0 0
             0 0 0
             0 0 -5
     '
-            =<svg.feMerge> @(
-                =<svg.feMergeNode>
-                =<svg.feMergeNode> -In 'SourceGraphic'
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
             )
         )
     
-        =<svg.text> "
+        svg.text "
     Embossed
     " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
     ) -ViewBox 0,0,300,100
