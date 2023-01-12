777c2c385924055997e8699238128c8822402f82
StartAutomating <StartAutomating@github.com>
2023-01-12 01:33:05 +0000

    Regenerating Examples (Fixes #95)

--- Commands/Standard/SVG.linearGradient.ps1
+++ Commands/Standard/SVG.linearGradient.ps1
@@ -5,54 +5,54 @@ function SVG.linearGradient {
 .Description
     The **`<linearGradient>`** element lets authors define linear gradients to apply to other SVG elements.
 .Example
-    =<svg> -Content @(
-        =<svg.defs> @(
-            =<svg.LinearGradient> -Id myGradient -Content @(
-                =<svg.stop> -Offset '10%' -Stopcolor gold
-                =<svg.stop> -Offset '95%' -Stopcolor red
+    svg -Content @(
+        svg.defs @(
+            svg.LinearGradient -Id myGradient -Content @(
+                svg.stop -Offset '10%' -Stopcolor gold
+                svg.stop -Offset '95%' -Stopcolor red
             )
         )
-        =<svg.circle> -Fill 'url(#myGradient)' -Cx 50 -Cy 50 -R 35
+        svg.circle -Fill 'url(#myGradient)' -Cx 50 -Cy 50 -R 35
     ) -viewbox 0,0,100,100
 .Example
-    =<svg> -Content @(
-        =<svg.defs> @(
-            =<svg.LinearGradient> -Id myGradient -Content @(
-                =<svg.stop> -Stopcolor gold @(
-                    =<svg.animate> -AttributeName offset -Values '.1;.99;.1' -Dur 5s -RepeatCount indefinite
+    svg -Content @(
+        svg.defs @(
+            svg.LinearGradient -Id myGradient -Content @(
+                svg.stop -Stopcolor gold @(
+                    svg.animate -AttributeName offset -Values '.1;.99;.1' -Dur 5s -RepeatCount indefinite
                 )
-                =<svg.stop> -Stopcolor red @(
-                    =<svg.animate> -AttributeName offset -Values '100;0;100' -Dur 5s -RepeatCount indefinite
+                svg.stop -Stopcolor red @(
+                    svg.animate -AttributeName offset -Values '100;0;100' -Dur 5s -RepeatCount indefinite
                 )
             )
         )
-        =<svg.rect> -Fill 'url(#myGradient)' -x 0 -Y 0 -Width 100 -Height 100
+        svg.rect -Fill 'url(#myGradient)' -x 0 -Y 0 -Width 100 -Height 100
     ) -ViewBox '0 0 100 100'
 .Example
-    =<svg> -Content @(
-        =<svg.defs> @(
-            =<svg.LinearGradient> -Id myGradient -Content @(
-                =<svg.stop> -Offset '10%' -Stopcolor transparent
-                =<svg.stop> -Offset '95%' -Stopcolor '#4488ff'
-                =<svg.animate> -AttributeName y1 -From 0 -To 1 -Id animateY1 -Fill freeze -Dur '3s'
-                =<svg.animate> -AttributeName y2 -Dur "3s" -From 1 -to 0 -Id 'animateY2' -Fill freeze -Begin 'animateY1.end'
-                =<svg.animate> -AttributeName x1 -Values '1;0' -Dur '3s' -Begin 'animateY2.end' -Fill freeze -Id animateX1
-                =<svg.animate> -AttributeName x2 -Values '0;1' -Dur '3s' -Begin 'animateX1.end' -Fill freeze
+    svg -Content @(
+        svg.defs @(
+            svg.LinearGradient -Id myGradient -Content @(
+                svg.stop -Offset '10%' -Stopcolor transparent
+                svg.stop -Offset '95%' -Stopcolor '#4488ff'
+                svg.animate -AttributeName y1 -From 0 -To 1 -Id animateY1 -Fill freeze -Dur '3s'
+                svg.animate -AttributeName y2 -Dur "3s" -From 1 -to 0 -Id 'animateY2' -Fill freeze -Begin 'animateY1.end'
+                svg.animate -AttributeName x1 -Values '1;0' -Dur '3s' -Begin 'animateY2.end' -Fill freeze -Id animateX1
+                svg.animate -AttributeName x2 -Values '0;1' -Dur '3s' -Begin 'animateX1.end' -Fill freeze
             ) -X1 100% -X2 0 -Y1 0% -Y2 100%
     
     
         )
-        =<svg.rect> -Fill 'url(#myGradient)' -Width 100 -Height 100
+        svg.rect -Fill 'url(#myGradient)' -Width 100 -Height 100
     ) -viewbox 0,0,100,100
 .Example
-    =<svg> -Content @(
-        =<svg.defs> @(
-            =<svg.LinearGradient> -Id myGradient -Content @(
-                =<svg.stop> -Offset '10%' -Stopcolor gold
-                =<svg.stop> -Offset '95%' -Stopcolor red
+    svg -Content @(
+        svg.defs @(
+            svg.LinearGradient -Id myGradient -Content @(
+                svg.stop -Offset '10%' -Stopcolor gold
+                svg.stop -Offset '95%' -Stopcolor red
             ) -X1 0 -X2 0 -Y1 0% -Y2 100%
         )
-        =<svg.rect> -Fill 'url(#myGradient)' -Width 100 -Height 100
+        svg.rect -Fill 'url(#myGradient)' -Width 100 -Height 100
     ) -viewbox 0,0,100,100
 .Link
     https://pssvg.start-automating.com/SVG.linearGradient
