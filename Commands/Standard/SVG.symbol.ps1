e2bee8fd67393f05d9323c5c374b0f938338bb28
StartAutomating <StartAutomating@github.com>
2023-07-03 19:26:10 +0000

    Regenerating (adding -Comment and fixing -Content)
    Fixes #112, re #114

--- Commands/Standard/SVG.symbol.ps1
+++ Commands/Standard/SVG.symbol.ps1
@@ -83,6 +83,11 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
+# # A comment that will appear before the element.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Comments')]
+[String]
+$Comment,
 # This attribute determines the height of the symbol.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','height')]
@@ -1502,9 +1507,14 @@ process {
             Attribute   = $attribute
         }
         # If content was provided
-        if ($content) {
+        if ($null -ne $content) {
             # put it into the splat.
             $writeSvgSplat.Content = $content
+        }
+        # If comments were provided
+        if ($comment) {
+            # put it into the splat.
+            $writeSvgSplat.Comment = $comment
         }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
