233b129adfd5ada611db3c5f3d3e70580cf3ac1d
StartAutomating <StartAutomating@github.com>
2023-07-03 19:26:08 +0000

    Regenerating (adding -Comment and fixing -Content)
    Fixes #112, re #114

--- Commands/Standard/SVG.feMerge.ps1
+++ Commands/Standard/SVG.feMerge.ps1
@@ -136,6 +136,11 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
+# # A comment that will appear before the element.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Comments')]
+[String]
+$Comment,
 # The **`id`** attribute assigns a unique name to an element.
 # 
 # You can use this attribute with any SVG element.
@@ -1467,9 +1472,14 @@ process {
             Attribute   = $attribute
         }
         # If content was provided
-        if ($content) {
+        if ($null -ne $content) {
             # put it into the splat.
             $writeSvgSplat.Content = $content
+        }
+        # If comments were provided
+        if ($comment) {
+            # put it into the splat.
+            $writeSvgSplat.Comment = $comment
         }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
