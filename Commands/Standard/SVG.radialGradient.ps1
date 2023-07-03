79404a46002a7c206070776482326a031e7e697d
StartAutomating <StartAutomating@github.com>
2023-07-03 19:26:09 +0000

    Regenerating (adding -Comment and fixing -Content)
    Fixes #112, re #114

--- Commands/Standard/SVG.radialGradient.ps1
+++ Commands/Standard/SVG.radialGradient.ps1
@@ -77,6 +77,11 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
+# # A comment that will appear before the element.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Comments')]
+[String]
+$Comment,
 # This attribute defines the x coordinate of the end circle of the radial gradient.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','cx')]
@@ -1526,9 +1531,14 @@ process {
             Attribute   = $attribute
         }
         # If content was provided
-        if ($content) {
+        if ($null -ne $content) {
             # put it into the splat.
             $writeSvgSplat.Content = $content
+        }
+        # If comments were provided
+        if ($comment) {
+            # put it into the splat.
+            $writeSvgSplat.Comment = $comment
         }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
