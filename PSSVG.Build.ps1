0b9ac8f30d6374ab8cecfa4ee1b37a5e903e2559
StartAutomating <StartAutomating@github.com>
2023-07-04 00:41:55 +0000

    Supporting -Children (Fixes #116)

--- PSSVG.Build.ps1
+++ PSSVG.Build.ps1
@@ -885,6 +885,12 @@ $OutputPath
             $writeSvgSplat.Comment = $comment
         }
 
+        # If any children were provided
+        if ($children) {
+            # put them in the splat.
+            $writeSvgSplat.Children = $children
+        }
+
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
             # put it into the splat.
