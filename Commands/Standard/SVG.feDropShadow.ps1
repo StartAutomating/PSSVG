5cb0965d39c2ad28c406900a71fe87fdcf5bf0a1
StartAutomating <StartAutomating@github.com>
2023-07-04 19:46:00 +0000

    Removing Multiline Comments from Examples (Fixes #122)

--- Commands/Standard/SVG.feDropShadow.ps1
+++ Commands/Standard/SVG.feDropShadow.ps1
@@ -1467,7 +1467,7 @@ process {
             # put it into the splat.
             $writeSvgSplat.On = $on
         }
-        Write-SVG @writeSvgSplat
+        . Write-SVG @writeSvgSplat
     
 }
 } 
