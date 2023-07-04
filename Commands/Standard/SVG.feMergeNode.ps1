b00e9761e856a14b1e841e157b6a99a7889abcaf
StartAutomating <StartAutomating@github.com>
2023-07-04 19:52:11 +0000

    Removing Excess Autogenerated Examples (Fixes #123)

--- Commands/Standard/SVG.feMergeNode.ps1
+++ Commands/Standard/SVG.feMergeNode.ps1
@@ -24,40 +24,6 @@ function SVG.feMergeNode {
     ) -ViewBox 0,0,300,100
 .Example
     svg @(
-        svg.filter -id embossed @(
-            svg.feConvolveMatrix -KernelMatrix '
-            5 0 0
-            0 0 0
-            0 0 -5
-    '
-            svg.feMerge @(
-                svg.feMergeNode
-                svg.feMergeNode -In 'SourceGraphic'
-            )
-        )
-    
-        svg.text "
-    Embossed
-    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
-    ) -ViewBox 0,0,300,100
-.Example
-    svg @(
-        svg.filter -id dropShadow @(
-            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
-                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
-            )
-            svg.feMerge @(
-                svg.feMergeNode
-                svg.feMergeNode -In 'SourceGraphic'
-            )
-        )
-    
-        svg.text "
-    Moving Shadows
-    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
-    ) -ViewBox 0,0,300,100
-.Example
-    svg @(
         svg.filter -id dropShadow @(
             svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
                 svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
