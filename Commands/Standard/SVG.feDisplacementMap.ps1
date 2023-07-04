af0a16f1ddb25c9d540208249566947936160719
StartAutomating <StartAutomating@github.com>
2023-07-04 19:46:00 +0000

    Removing Multiline Comments from Examples (Fixes #122)

--- Commands/Standard/SVG.feDisplacementMap.ps1
+++ Commands/Standard/SVG.feDisplacementMap.ps1
@@ -1483,7 +1483,7 @@ process {
             # put it into the splat.
             $writeSvgSplat.On = $on
         }
-        Write-SVG @writeSvgSplat
+        . Write-SVG @writeSvgSplat
     
 }
 } 
