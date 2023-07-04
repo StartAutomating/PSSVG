d7fc2cdefbf5e572ee9c96db82d890c4880b4e05
StartAutomating <StartAutomating@github.com>
2023-07-04 00:41:59 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.pattern.ps1
+++ Commands/Standard/SVG.pattern.ps1
@@ -1644,6 +1644,11 @@ process {
             # put it into the splat.
             $writeSvgSplat.Comment = $comment
         }
+        # If any children were provided
+        if ($children) {
+            # put them in the splat.
+            $writeSvgSplat.Children = $children
+        }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
             # put it into the splat.
