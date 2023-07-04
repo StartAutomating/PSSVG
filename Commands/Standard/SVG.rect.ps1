3ca9d429ee37087119888526fa898823f75ed350
StartAutomating <StartAutomating@github.com>
2023-07-04 19:52:12 +0000

    Removing Excess Autogenerated Examples (Fixes #123)

--- Commands/Standard/SVG.rect.ps1
+++ Commands/Standard/SVG.rect.ps1
@@ -476,12 +476,6 @@ function SVG.rect {
             }
     )
 .Example
-    =<svg> @(
-        =<svg.circle> -CX 25 -CY 25 -r 10 -Fill '#4488ff'
-        =<svg.rect> -X 0 -Y 50 -Width 20 -Height 20 -Fill '#4488ff'
-        =<svg.ellipse> -Cx 25 -Cy 100 -Rx 10 -Ry 5 -Fill '#4488ff'
-    ) -ViewBox 0, 0, 100, 200
-.Example
     SVG -ViewBox 1.986,1 -Content @(
         $g = (1.986 * .4) / 12
         $e = (7/13)/10
