757395d8e745290b875bfbc3ad6258d938d6e5ec
StartAutomating <StartAutomating@github.com>
2023-07-04 00:41:55 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.animateMotion.ps1
+++ Commands/Standard/SVG.animateMotion.ps1
@@ -529,6 +529,11 @@ process {
             # put it into the splat.
             $writeSvgSplat.Comment = $comment
         }
+        # If any children were provided
+        if ($children) {
+            # put them in the splat.
+            $writeSvgSplat.Children = $children
+        }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
             # put it into the splat.
