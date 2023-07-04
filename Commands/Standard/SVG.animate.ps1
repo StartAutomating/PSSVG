264112f2927914460627a4cc0e55a00bc840c0ad
StartAutomating <StartAutomating@github.com>
2023-07-04 20:30:30 +0000

    Adding BPM Triangle Morph Example

--- Commands/Standard/SVG.animate.ps1
+++ Commands/Standard/SVG.animate.ps1
@@ -1362,6 +1362,29 @@ function SVG.animate {
         SVG.rect -Width 800 -Height 800 -Fill "url(#fillPattern)" -X 0 -Y 0
     )
 .Example
+    $bpm = 128
+    
+    $animateDuration = "$([Math]::Round((60/$bpm)*2, 4))s"
+    $animateSplat = [Ordered]@{
+        Dur = $animateDuration
+        RepeatDur = "indefinite"
+    }
+    
+    $Scale = 8
+    
+    $patternSize = 1/$scale
+    
+    $Color = "#4488ff"
+    
+    SVG -ViewBox 800, 800 @(
+        SVG.pattern -Width $patternSize -Height $patternSize @(
+            SVG.polygon -Points "0,0, 100,100, 0,100" @(
+                SVG.animate -AttributeName points -to "100,100 0,0 100,0" -AttributeType XML @animateSplat
+            )  -Fill $color -Stroke $color
+        ) -id fillPattern
+        SVG.rect -Width 800 -Height 800 -Fill "url(#fillPattern)" -X 0 -Y 0
+    )
+.Example
     $AnimationTimeframe = [Ordered]@{
         Dur = '2s'
         RepeatCount = 'indefinite'
