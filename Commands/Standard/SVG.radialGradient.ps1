709ef9390b392b8f0a577f4a1e393c7ecb76370b
StartAutomating <StartAutomating@github.com>
2023-07-04 00:41:59 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.radialGradient.ps1
+++ Commands/Standard/SVG.radialGradient.ps1
@@ -1544,6 +1544,11 @@ process {
             # put it into the splat.
             $writeSvgSplat.Comment = $comment
         }
+        # If any children were provided
+        if ($children) {
+            # put them in the splat.
+            $writeSvgSplat.Children = $children
+        }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
             # put it into the splat.
