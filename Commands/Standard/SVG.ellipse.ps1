416eb9a29d025914b23709f9dfd588d250e3f52b
StartAutomating <StartAutomating@github.com>
2023-07-04 00:28:30 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.ellipse.ps1
+++ Commands/Standard/SVG.ellipse.ps1
@@ -60,11 +60,15 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
-# # A comment that will appear before the element.
+# A comment that will appear before the element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('Comments')]
 [String]
 $Comment,
+# One or more child elements.  These will be treated as if they were content.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Child')]
+$Children,
 # The x position of the center of the ellipse.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','cx')]
