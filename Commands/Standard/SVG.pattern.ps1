30b183f20e67fa540d7ee237946b4d1bac475fdc
StartAutomating <StartAutomating@github.com>
2023-07-04 19:46:02 +0000

    Removing Multiline Comments from Examples (Fixes #122)

--- Commands/Standard/SVG.pattern.ps1
+++ Commands/Standard/SVG.pattern.ps1
@@ -1664,7 +1664,7 @@ process {
             # put it into the splat.
             $writeSvgSplat.On = $on
         }
-        Write-SVG @writeSvgSplat
+        . Write-SVG @writeSvgSplat
     
 }
 } 
