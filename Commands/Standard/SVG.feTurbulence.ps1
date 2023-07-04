2911e843d7c4242d61599bc8faa2f5bcb305ead2
StartAutomating <StartAutomating@github.com>
2023-07-04 00:28:32 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.feTurbulence.ps1
+++ Commands/Standard/SVG.feTurbulence.ps1
@@ -67,11 +67,15 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
-# # A comment that will appear before the element.
+# A comment that will appear before the element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('Comments')]
 [String]
 $Comment,
+# One or more child elements.  These will be treated as if they were content.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Child')]
+$Children,
 # 
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','baseFrequency')]
