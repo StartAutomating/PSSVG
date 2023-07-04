4037e684ba5c8c1a65fc58a2d51c6ee590662bfa
StartAutomating <StartAutomating@github.com>
2023-07-04 00:41:58 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.foreignObject.ps1
+++ Commands/Standard/SVG.foreignObject.ps1
@@ -1502,6 +1502,11 @@ process {
             # put it into the splat.
             $writeSvgSplat.Comment = $comment
         }
+        # If any children were provided
+        if ($children) {
+            # put them in the splat.
+            $writeSvgSplat.Children = $children
+        }
         # If we provided an -OutputPath
         if ($paramCopy['OutputPath']) {
             # put it into the splat.
