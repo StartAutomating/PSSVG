5eba373d9b920035e3b4345a527753e02998a929
StartAutomating <StartAutomating@github.com>
2023-07-04 00:28:33 +0000

    Supporting -Children (Fixes #116)

--- Commands/Standard/SVG.symbol.ps1
+++ Commands/Standard/SVG.symbol.ps1
@@ -83,11 +83,15 @@ $On,
 [Alias('SVGAttributes','SVGAttribute')]
 [Collections.IDictionary]
 $Attribute = [Ordered]@{},
-# # A comment that will appear before the element.
+# A comment that will appear before the element.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('Comments')]
 [String]
 $Comment,
+# One or more child elements.  These will be treated as if they were content.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias('Child')]
+$Children,
 # This attribute determines the height of the symbol.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','height')]
