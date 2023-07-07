0d8724a3796c5b34bbfdb9540462505dde98ef9c
StartAutomating <StartAutomating@github.com>
2023-07-07 20:11:16 +0000

    Write-SVG - Fixing -ViewBox (when strings) (Fixes #127)

--- Commands/Standard/SVG.rect.ps1
+++ Commands/Standard/SVG.rect.ps1
@@ -457,7 +457,7 @@ function SVG.rect {
     # The total duration of any animations.
     [timespan]$duration = '00:00:03.75',
     # A palette of colors to alternate thru
-    [string[]]$colors = @('#112244','#224488',"#4488ff"),
+    [string[]]$Color = @('#112244','#224488',"#4488ff"),
     # The type of the shape. (either Star or ConvexPolygon)
     [ValidateSet("Star", "ConvexPolygon")]
     [string]
@@ -480,14 +480,14 @@ function SVG.rect {
         SVG.rect -Width 1000% -Height 1000% -X -500% -Y -500% -Fill 'black'
     
         0..($RepeatCount -1) |
-            . $shapeCommand @Splat -Rotate {
+            & $shapeCommand @Splat -Rotate {
                     $_ * ($totalRotation / $RepeatCount)
             } -Radius {
                 $Radius - (
                     $_ * ($Radius / $RepeatCount)
                 )
             } -Stroke {
-                $colors[$_ % $colors.Length]
+                $Color[$_ % $color.Length]
             } -Children {
                 $toRotation =  $(360 * ([Math]::Ceiling(($_ + 1)/10)))
                 SVG.animateTransform -From "0 $centerX $centerY" -To "$toRotation $centerX $centerY" -Dur $duration -AttributeName transform -Type 'rotate' -RepeatCount 'indefinite'
