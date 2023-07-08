cb11fe6d6f59a5e7acecd10d6343d5bcdaa93625
StartAutomating <StartAutomating@github.com>
2023-07-08 04:01:43 +0000

    Updating Module Version [0.2.10] and CHANGELOG
    Also, running GitLogger last (re #134)

--- Commands/Standard/SVG.rect.ps1
+++ Commands/Standard/SVG.rect.ps1
@@ -375,6 +375,18 @@ function SVG.rect {
         SVG.rect -x '0' -y '0' -width 100% -height 100% -style 'filter: url(#noise1);' -Fill '#4488ff' -Opacity .2
     )
 .Example
+    SVG -Content @(
+        SVG.defs -Content @(
+    
+            SVG.pattern -id 'HexagonPattern' -patternUnits 'userSpaceOnUse' -width '174' -height '200' -patternTransform 'scale(.5)' -Content @(
+                SVG.Hexagon -CenterX 87 -CenterY 100 -Radius 100 -Fill transparent -Stroke '#4488ff' -Comment "A hexagon with a" -Class 'foreground-fill'
+            ) -Comment "A faint hexagon Pattern, rendered 4x at a base scale of 174 by 200"
+        )
+        $hugeSize = 20000
+    
+        SVG.rect -width "$hugeSize%" -height "$hugeSize%" -fill 'url(#HexagonPattern)'
+    )
+.Example
     svg -Content @(
         svg.defs @(
             svg.LinearGradient -Id myGradient -Content @(
