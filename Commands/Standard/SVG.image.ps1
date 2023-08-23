a78ac299cc556413709fdc0855bfa40ae61561db
StartAutomating <StartAutomating@github.com>
2023-08-23 00:08:44 +0000

    Regenerating

--- Commands/Standard/SVG.image.ps1
+++ Commands/Standard/SVG.image.ps1
@@ -118,6 +118,11 @@ $PreserveAspectRatio,
 [Reflection.AssemblyMetaData('SVG.AttributeName','crossorigin')]
 [PSObject]
 $Crossorigin,
+# 
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','decoding')]
+[PSObject]
+$Decoding,
 # The **`requiredFeatures`** attribute takes a list of feature strings, with the individual strings separated by white space. It determines whether or not all of the named features are supported by the browser; if all of them are supported, the attribute evaluates to `true` end the element is rendered; otherwise, the attribute evaluates to `false` and the current element and its children are skipped and thus will not be rendered. This provides a way to design SVG that gracefully falls back when features aren't available.
 # 
 # If the attribute is not present, then its implicit evaluated value is `true`. If a null string or empty string value is given to attribute `requiredFeatures`, the attribute is evaluate to `false`.
