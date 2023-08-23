0f4f9cf69a60018b0cf35d9bd003a7f9ca2d91a8
StartAutomating <StartAutomating@github.com>
2023-08-23 00:08:44 +0000

    Regenerating

--- Commands/Standard/SVG.feColorMatrix.ps1
+++ Commands/Standard/SVG.feColorMatrix.ps1
@@ -17,7 +17,7 @@ function SVG.feColorMatrix {
     
     In simplified terms, below is how each color channel in the new pixel is calculated. The last row is ignored because its values are constant.
     
-    ```
+    ```plain
     R' = r1*R + r2*G + r3*B + r4*A + r5
     G' = g1*R + g2*G + g3*B + g4*A + g5
     B' = b1*R + b2*G + b3*B + b4*A + b5
