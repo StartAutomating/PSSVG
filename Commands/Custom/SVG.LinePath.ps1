1ddb2345628f957f6f293a8e129a2079b8b78a29
StartAutomating <StartAutomating@github.com>
2023-12-24 22:23:27 +0000

    feat: SVG.LinePath -Angle ( Fixes #143 )

--- Commands/Custom/SVG.LinePath.ps1
+++ Commands/Custom/SVG.LinePath.ps1
@@ -6,16 +6,23 @@ function SVG.LinePath {
     .DESCRIPTION    
         Draws a line in SVG Path Syntax.    
     .EXAMPLE    
-        =<svg> -Viewbox 100, 100 (    
-            =<svg.LinePath> -Start 50 -End 75 -Stroke black    
+        svg -Viewbox 100, 100 (    
+            svg.LinePath -Start 50 -End 75 -Stroke black    
         ) -OutputPath .\Line1.svg    
     .EXAMPLE    
-        =<svg> -Viewbox 60, 60 (    
-            =<svg.LinePath> -Start 10 -Horizontal -End 50 -Stroke black -Fill transparent |    
-                =<svg.LinePath> -Vertical -End 50 |    
-                =<svg.LinePath> -Horizontal -End 10 |    
-                =<svg.LinePath> -Vertical -End 10 -Close    
-        ) -OutputPath .\LineBox.svg     
+        svg -Viewbox 60, 60 (    
+            svg.LinePath -Start 10 -Horizontal -End 50 -Stroke black -Fill transparent |    
+                svg.LinePath -Vertical -End 50 |    
+                svg.LinePath -Horizontal -End 10 |    
+                svg.LinePath -Vertical -End 10 -Close    
+        ) -OutputPath .\LineBox.svg    
+    .EXAMPLE    
+        svg -Viewbox 100 (        
+            svg.LinePath -Start 50 -End 10 -Angle 45 -Stroke black -Fill transparent |    
+                svg.LinePath -End 10 -Angle -45 -Stroke black -Fill transparent |    
+                svg.LinePath -End 15 -Angle 50 -Stroke black -Fill transparent |    
+                svg.LinePath -End 15 -Angle -50 -Stroke black -Fill transparent    
+        ) -OutputPath .\ZigZagLinePath.svg    
     .LINK    
         SVG.Path    
     
@@ -46,11 +53,19 @@ function SVG.LinePath {
     [switch]
     $Close,
 
-    # The end point of the line, or it's length    
-    # If this is provided, a line will be drawn to this point    
-    [Parameter(ValueFromPipelineByPropertyName)]    
+    # The end point of the line, or it's length.    
+    # If this is provided without `-Angle`, a line will be drawn to this point.    
+    # If this is provided with `-Angle`, a line of this `-Distance` be drawn.    
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('Distance')]
     [double[]]
-    $End
+    $End,
+
+    # An optional angle.    
+    # If an `-Angle` is provided and `-End` is a single number, it will be treated as a `-Distance`.    
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [double]
+    $Angle
     )
     dynamicParam {
     $baseCommand = 
@@ -104,12 +119,16 @@ function SVG.LinePath {
                     Write-Error "-Start can only contain one or two values"
                     return
                 }
-                elseif ($start.Length -eq 2) {
-                    $start[0],$start[1]
-                }
-                else {
-                    $start[0],$start[0]
-                }
+                $startPoint = 
+                    if ($start.Length -eq 2) {
+                        $start[0],$start[1]
+                    }
+                    else {
+                        $start[0],$start[0]
+                    }
+                $startPoint
+            } else {
+                $startPoint = @($_.D -split '\s' -match '[\d\.]+')[-1..-2] -as [double[]]
             }
             if ($Vertical) {
                 "V $(if ($end) { $end[0] } else { 0 })"
@@ -133,8 +152,20 @@ function SVG.LinePath {
                     "L"
                     $end
                 } elseif ($end.Length -eq 1) {
+                    if ($PSBoundParameters['Angle']) {
+                        if ($startPoint) {
+                            0.0 + $startPoint[0] + ($end[0] * [math]::round([math]::sin($angle * [Math]::PI/180),15))
+                            0.0 + $startPoint[1] + ($end[0] * [math]::round([math]::cos($angle * [Math]::PI/180),15))
+                        } 
+                        else
+                        {
+                            $end[0] * [math]::round([math]::sin($angle * [Math]::PI/180),15)
+                            $end[0] * [math]::round([math]::cos($angle * [Math]::PI/180),15)
+                        } 
+                    } else {
                     "L"
                     $end * 2
+                    }
                 }
                 
             }
