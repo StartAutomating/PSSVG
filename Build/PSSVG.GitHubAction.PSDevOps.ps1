70962db0330697a41ce653deab3a148761320966
James Brundage <@github.com>
2024-02-19 15:31:20 -0800

    chore: Moving Action/Workflow definitions into /Build ( Fixes #155 )

--- Build/PSSVG.GitHubAction.PSDevOps.ps1
+++ Build/PSSVG.GitHubAction.PSDevOps.ps1
@@ -1,6 +1,13 @@
 ﻿#requires -Module PSDevOps
-#requires -Module PSSVG
-Import-BuildStep -ModuleName PSSVG
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubAction
+
+Push-Location ($PSScriptRoot | Split-Path)
+
 New-GitHubAction -Name "MakePSSVG" -Description @'
 Build SVGs in your Workflow (with PowerShell)
 '@ -Action PSSVG -Icon aperture -OutputPath .\action.yml 
+
+
+Pop-Location
--- Build/PSSVG.GitHubWorkflow.PSDevOps.ps1
+++ Build/PSSVG.GitHubWorkflow.PSDevOps.ps1
@@ -1,5 +1,10 @@
 #requires -Module PSDevOps
-Import-BuildStep -ModuleName PSSVG
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubWorkflow
+
+Push-Location ($PSScriptRoot | Split-Path)
+
 New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, MakePSSVG -Environment @{
     NoCoverage = $true
     GIT_TOKEN = '${{secrets.GITHUB_TOKEN}}'
@@ -10,3 +15,6 @@ New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest
 New-GitHubWorkflow -On Demand -Job RunGitPub -Name GitPub -OutputPath (
     Join-Path $PSScriptRoot .github\workflows\GitPub.yml
 )
+
+
+Pop-Location
