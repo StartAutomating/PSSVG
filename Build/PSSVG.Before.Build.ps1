4e77ef20ffa2829e45d8e362a99e20c2df0ca60c
StartAutomating <StartAutomating@github.com>
2024-02-19 23:47:56 +0000

    fix: Fixing EZOut Pathing ( Fixes #155 )

---
+++ Build/PSSVG.Before.Build.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    Runs before PSSVGs main build
+.DESCRIPTION
+    Runs before the main PSSVG build.
+.NOTES
+    This will import PSSVG for later build steps.
+#>
+param()
+
+Push-Location ($PSScriptRoot | Split-Path)
+
+Import-Module .\PSSVG.psd1 -Force -Global
+
+Pop-Location
+
