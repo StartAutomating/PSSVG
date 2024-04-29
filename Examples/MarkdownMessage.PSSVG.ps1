4757c577e45bf38adfcf061c6b0b2f6a8c407743
James Brundage <@github.com>
2024-04-28 17:14:02 -0700

    docs: Adding MarkdownMessage Example ( re #172 )

---
+++ Examples/MarkdownMessage.PSSVG.ps1
@@ -0,0 +1,27 @@
+#requires -Module PSSVG
+param(
+# The ViewBox of the SVG
+[int[]]
+$ViewBox = @(1920, 1080),
+
+# The font size
+[string]
+$FontSize = "3em",
+
+# The google font to use
+[string]
+$GoogleFont,
+
+# The markdown to convert to SVG
+[string]
+$Markdown = "# Hello World"
+)
+
+svg -ViewBox $ViewBox -Data $Data -FontSize $FontSize @(
+    if ($GoogleFont) {
+        SVG.GoogleFont -Font $GoogleFont
+    }
+    $MarkdownParameters = [Ordered]@{}
+    if ($GoogleFont) { $MarkdownParameters.FontFamily = $GoogleFont}
+    SVG.Markdown -Markdown $Markdown @MarkdownParameters
+) -OutputPath (Join-Path $PSScriptRoot .\Markdown.svg)
