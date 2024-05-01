162fd720696d9d321b78e5392f2f59bcf002d481
James Brundage <@github.com>
2024-04-30 19:16:30 -0700

    docs: Rose Example ( re #140 )

---
+++ Examples/Rose.PSSVG.ps1
@@ -0,0 +1,22 @@
+#requires -Module PSSVG
+param(
+# The radius of the rose.
+[double]
+$Radius,
+
+# The frequency of the rose.
+[double]
+$Frequency,
+
+# The number of revolutions to draw.
+[double]
+$RevolutionCount,
+
+# The stroke color of the rose.
+[string]
+$Color
+)
+
+SVG -ViewBox ($Radius * 2) @(
+    SVG.Rose -Radius $Radius -Frequency $Frequency -RevolutionCount $RevolutionCount -Stroke $Color -Center $Radius
+) -OutputPath .\Rose.svg
