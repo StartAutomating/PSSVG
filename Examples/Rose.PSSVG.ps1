82143f60d8190aee5e4378a837a733be8cdb6cb2
James Brundage <@github.com>
2024-04-30 21:19:41 -0700

    docs: Rose Example ( re #140 )
    Adding default values

--- Examples/Rose.PSSVG.ps1
+++ Examples/Rose.PSSVG.ps1
@@ -3,21 +3,25 @@
 param(
 # The radius of the rose.
 [double]
-$Radius,
+$Radius = 100,
 
 # The frequency of the rose.
 [double]
-$Frequency,
+$Frequency = 2,
+
+# The initial rotation of the rose.
+[double]
+$Rotate = 0,
 
 # The number of revolutions to draw.
 [double]
-$RevolutionCount,
+$RevolutionCount = 1,
 
 # The stroke color of the rose.
 [string]
-$Color
+$Color = '#4488ff'
 )
 
 SVG -ViewBox ($Radius * 2) @(
-    SVG.Rose -Radius $Radius -Frequency $Frequency -RevolutionCount $RevolutionCount -Stroke $Color -CenterX $Radius
+    SVG.Rose -Radius $Radius -Frequency $Frequency -RevolutionCount $RevolutionCount -Stroke $Color -CenterX $Radius -Rotate $Rotate
 ) -OutputPath .\Rose.svg
