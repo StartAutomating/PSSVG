c9527b768f0a7cdfa1aab01f65cf29479ae315b5
StartAutomating <StartAutomating@github.com>
2024-05-01 17:51:14 +0000

    chore: Adding Build Patterns ( Fixes #157 )
        to SVG.Star, SVG.Rhombus, and SVG.ConvexPolygon

--- Commands/Shapes/SVG.Rhombus.ps1
+++ Commands/Shapes/SVG.Rhombus.ps1
@@ -1,3 +1,5 @@
+[ValidatePattern('SVG\.Rhombus')]
+param()
 function SVG.Rhombus {
     <#
     
@@ -26,7 +28,7 @@ function SVG.Rhombus {
     
     #>
             
-    [Alias('SVG.Rhombi')]    
+    [Alias('SVG.Rhombi','SVG.Rhombuses')]    
     [CmdletBinding(PositionalBinding=$false)]    
 
     param(
@@ -1688,16 +1690,14 @@ $WritingMode
             $centerX + $Radius * [math]::round([math]::sin($CurrentAngle * [Math]::PI/180),15)
             # Right
             $CurrentAngle += 90
-            "L"            
+
             $centerY + $InRadius * [math]::round([math]::cos($CurrentAngle * [Math]::PI/180),15)
             $centerX + $InRadius * [math]::round([math]::sin($CurrentAngle * [Math]::PI/180),15)
             # Bottom
-            "L"
             $CurrentAngle += 90
             $centerY + $Radius * [math]::round([math]::cos($CurrentAngle * [Math]::PI/180),15)
             $centerX + $Radius * [math]::round([math]::sin($CurrentAngle * [Math]::PI/180),15)
             # Left
-            "L"
             $CurrentAngle += 90
             $centerY + $InRadius * [math]::round([math]::cos($CurrentAngle * [Math]::PI/180),15)
             $centerX + $InRadius * [math]::round([math]::sin($CurrentAngle * [Math]::PI/180),15)
