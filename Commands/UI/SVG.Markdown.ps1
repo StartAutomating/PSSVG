edf95a4b5d72f94ab4bb0696d1350dfb3721d677
StartAutomating <StartAutomating@github.com>
2024-05-10 07:53:01 +0000

    feat: SVG.Markdown ( Fixes #172 )
        Enabling disabled input (task list items)

--- Commands/UI/SVG.Markdown.ps1
+++ Commands/UI/SVG.Markdown.ps1
@@ -126,8 +126,7 @@ function SVG.Markdown {
         $svgSplat = [Ordered]@{} + $PSBoundParameters
         $svgForeignSplat = [Ordered]@{} + $PSBoundParameters
         
-        # and strip off any parameters that are not applicable to the base command
-        $myCommandMetadata = $MyInvocation.MyCommand
+        # and strip off any parameters that are not applicable to the base command        
         $svgCmd = $baseCommand
         foreach ($parameterName in @($svgSplat.Keys)) {
             if (-not $svgForeignObject.Parameters[$parameterName]) {
@@ -156,6 +155,7 @@ function SVG.Markdown {
         $nodeIndex = 0
         foreach ($node in $allNodesInOrder) {
             if ($node.SetAttribute) { $node.SetAttribute('data-index', $nodeIndex) }
+            if ($node.GetAttribute('disabled')) { $node.RemoveAttribute('disabled') }
             $nodeIndex++
         }        
         
