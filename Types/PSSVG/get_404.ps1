78231f5609e07120c187050d8ba8054c6d8b6b06
James Brundage <@github.com>
2024-05-14 11:48:27 -0700

    feat: PSSVG.404 ( Fixes #188 )

---
+++ Types/PSSVG/get_404.ps1
@@ -0,0 +1,4 @@
+SVG -ViewBox 900 @(
+    SVG.Octagon -Stroke '#4488ff' -Radius 300 -CenterX 450 -CenterY 450 -Fill transparent
+    SVG.Text -X 50% -Y 50% -DominantBaseline 'middle' -AlignmentBaseline 'middle' -TextAnchor 'middle' -Fontsize 100 -Fill '#4488ff' -Content '404' 
+)
