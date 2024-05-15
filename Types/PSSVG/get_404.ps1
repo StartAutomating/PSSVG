bbd345e5d5346db476bf8f810885b1b77c8adb75
James Brundage <@github.com>
2024-05-15 13:39:03 -0700

    fix: PSSVG.Serve 404s ( Fixes #188 )
    Rotating Octogon, animating status code

--- Types/PSSVG/get_404.ps1
+++ Types/PSSVG/get_404.ps1
@@ -1,4 +1,13 @@
 SVG -ViewBox 900 @(
-    SVG.Octagon -Stroke '#4488ff' -Radius 300 -CenterX 450 -CenterY 450 -Fill transparent
-    SVG.Text -X 50% -Y 50% -DominantBaseline 'middle' -AlignmentBaseline 'middle' -TextAnchor 'middle' -Fontsize 100 -Fill '#4488ff' -Content '404' 
+    
+    SVG.Octagon -Stroke '#4488ff' -Radius (
+        400
+    ) -CenterX 450 -CenterY 450 -Fill transparent -Rotate (
+        360/16
+    ) -StrokeWidth 0.5%    
+    
+    SVG.Text -X 50% -Y 50% -DominantBaseline 'middle' -AlignmentBaseline 'middle' -TextAnchor 'middle' -Fontsize 100 -Fill '#4488ff' -Content @(
+        (SVG.tspan -Content '404').OuterXml
+        SVG.Animate -AttributeName 'font-size' -Values '100;110;100' -Dur '1s' -RepeatCount 'indefinite'
+    )
 )
