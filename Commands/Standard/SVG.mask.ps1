47f577307b4889c7606b0fc70d9345f61c555fa6
StartAutomating <StartAutomating@github.com>
2024-06-11 19:02:13 +0000

    feat: Forcing all MDN elements to have -ID ( Fixes #217 )
        Also, not transpiling generating functions

--- Commands/Standard/SVG.mask.ps1
+++ Commands/Standard/SVG.mask.ps1
@@ -1,5 +1,4 @@
-function SVG.mask {
-
+function SVG.mask  {
 <#
 .Synopsis
     Creates SVG mask elements
@@ -43,6 +42,10 @@ param(
 [Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
+# The element identifier.
+[Parameter(ValueFromPipelineByPropertyName)]
+[string]
+$Id,
 # A dictionary containing data.  This data will be embedded in data- attributes.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('DataAttribute','DataAttributes')]
@@ -231,6 +234,10 @@ process {
             }
         }
 
+        if ($paramCopy.Id) {
+            $attribute.Id = $paramCopy.Id
+        }
+
         # All commands will call Write-SVG.  Prepare a splat.
         $writeSvgSplat = @{
             ElementName = $elementName
@@ -275,12 +282,11 @@ process {
 
         if ($slot) {
             $writeSvgSplat.Slot = $slot
-        }
+        }        
 
         . Write-SVG @writeSvgSplat
     
 }
-
 
 } 
 
