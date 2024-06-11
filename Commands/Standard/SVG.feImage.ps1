c9ffd61fb9d249bcbe4ef264139a0b2e6c76449d
StartAutomating <StartAutomating@github.com>
2024-06-11 19:02:13 +0000

    feat: Forcing all MDN elements to have -ID ( Fixes #217 )
        Also, not transpiling generating functions

--- Commands/Standard/SVG.feImage.ps1
+++ Commands/Standard/SVG.feImage.ps1
@@ -1,5 +1,4 @@
-function SVG.feImage {
-
+function SVG.feImage  {
 <#
 .Synopsis
     Creates SVG feImage elements
@@ -20,6 +19,10 @@ param(
 [Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
+# The element identifier.
+[Parameter(ValueFromPipelineByPropertyName)]
+[string]
+$Id,
 # A dictionary containing data.  This data will be embedded in data- attributes.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('DataAttribute','DataAttributes')]
@@ -110,6 +113,10 @@ process {
             }
         }
 
+        if ($paramCopy.Id) {
+            $attribute.Id = $paramCopy.Id
+        }
+
         # All commands will call Write-SVG.  Prepare a splat.
         $writeSvgSplat = @{
             ElementName = $elementName
@@ -154,12 +161,11 @@ process {
 
         if ($slot) {
             $writeSvgSplat.Slot = $slot
-        }
+        }        
 
         . Write-SVG @writeSvgSplat
     
 }
-
 
 } 
 
