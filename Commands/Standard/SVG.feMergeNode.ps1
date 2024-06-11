e9181b62722104696b0c89ae844b42e720a0c1d0
StartAutomating <StartAutomating@github.com>
2024-06-11 18:44:23 +0000

    feat: Write-SVG -Slot and Regenerating from MDN ( Fixes #217 )

--- Commands/Standard/SVG.feMergeNode.ps1
+++ Commands/Standard/SVG.feMergeNode.ps1
@@ -6,13 +6,34 @@ function SVG.feMergeNode {
 .Description
     The `feMergeNode` takes the result of another filter to be processed by its parent `feMerge`.
 .Example
+    param(
+    # The message to emboss
+    [string]
+    $Message = "Embossed",
+    
+    # The fill color of the text
+    [string]
+    $Fill = '#4488ff',
+    
+    # The css classes to apply to the image
+    [string[]]
+    $Class,
+    
+    # The font size of the text
+    [string]
+    $FontSize = "26em",
+    
+    # The embossing level
+    [int]
+    $Emboss = 5
+    )
     svg @(
         svg.filter -id embossed @(
-            svg.feConvolveMatrix -KernelMatrix '
-            5 0 0
+            svg.feConvolveMatrix -KernelMatrix "
+            $Emboss 0 0
             0 0 0
-            0 0 -5
-    '
+            0 0 -$($Emboss * -1)
+    "
             svg.feMerge @(
                 svg.feMergeNode
                 svg.feMergeNode -In 'SourceGraphic'
@@ -21,13 +42,31 @@ function SVG.feMergeNode {
     
         svg.text "
     Embossed
-    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#embossed)'
-    ) -ViewBox 0,0,300,100
+    " -TextAnchor middle -DominantBaseline middle -Fill $Fill -FontSize $FontSize -X 50% -Y 50% -Filter 'url(#embossed)'
+    ) -Class $class -ViewBox 0,0,300,100
 .Example
+    param(
+    [string]
+    $Fill = '#4488ff',
+    
+    [string[]]
+    $Class,
+    
+    [timespan]
+    $Duration = '00:00:02.5',
+    
+    [double[]]
+    $ShadowX = @(0.5, -.5, 0.5),
+    
+    [double[]]
+    $ShadowY = @(0.75, -.75, 0.75)
+    )
+    
     svg @(
         svg.filter -id dropShadow @(
-            svg.feDropShadow -dx 0.5 -dy 0.75 -StdDeviation 0 @(
-                svg.animate -AttributeName dx -Values '.5;-.5;.5' -Dur 1s -RepeatCount 'indefinite'
+            svg.feDropShadow -dx $ShadowX[0] -dy $ShadowY[0] -StdDeviation 0 @(
+                svg.animate -AttributeName dx -Values $($ShadowX -join ';') -Dur $Duration -RepeatCount 'indefinite'
+                svg.animate -AttributeName dY -Values $($Shadowy -join ';') -Dur $Duration -RepeatCount 'indefinite'
             )
             svg.feMerge @(
                 svg.feMergeNode
@@ -37,7 +76,7 @@ function SVG.feMergeNode {
     
         svg.text "
     Moving Shadows
-    " -TextAnchor middle -DominantBaseline middle -Fill '#4488ff' -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
+    " -TextAnchor middle -DominantBaseline middle -Fill $fill -Class $Class -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
     ) -ViewBox 0,0,300,100
 .Link
     https://pssvg.start-automating.com/SVG.feMergeNode
@@ -63,7 +102,13 @@ $Data,
 # Each key or property name will be the name of the event
 # Each value will be the handler.
 [Parameter(ValueFromPipelineByPropertyName)]
+[PSObject]
 $On,
+# The slot attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias("SlotName")]
+[string]
+$Slot,
 # A dictionary of attributes.  This can set any attribute not exposed in other parameters.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('SVGAttributes','SVGAttribute')]
@@ -106,88 +151,7 @@ $Children,
 [Reflection.AssemblyMetaData('SVG.Default value', 'SourceGraphic for first filter primitive, otherwise result of previous filter primitive')]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
 [PSObject]
-$In,
-# The **`id`** attribute assigns a unique name to an element.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','id')]
-[Reflection.AssemblyMetaData('SVG.Value', '<id>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Id,
-# The **`lang`** attribute specifies the primary language used in contents and attributes containing text content of particular elements.
-# 
-# There is also an xml:lang attribute (with namespace). If both of them are defined, the one with namespace is used and the one without is ignored.
-# 
-# In SVG 1.1 there was a `lang` attribute defined with a different meaning and only applying to glyph elements. That attribute specified a list of languages according to {{RFC(5646, "Tags for Identifying Languages (also known as BCP 47)")}}. The glyph was meant to be used if the `xml:lang` attribute exactly matched one of the languages given in the value of this parameter, or if the `xml:lang` attribute exactly equaled a prefix of one of the languages given in the value of this parameter such that the first tag character following the prefix was "-".
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','lang')]
-[Reflection.AssemblyMetaData('SVG.Value', '<language-tag>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Lang,
-# The **`tabindex`** attribute allows you to control whether an element is focusable and to define the relative order of the element for the purposes of sequential focus navigation.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','tabindex')]
-[Reflection.AssemblyMetaData('SVG.Value', 'valid integer')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Tabindex,
-# The **`xml:base`** attribute specifies a base IRI other than the base IRI of the document or external entity.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:base')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', '<iri>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlBase,
-# The **`xml:lang`** attribute specifies the primary language used in contents and attributes containing text content of particular elements.
-# 
-# It is a universal attribute allowed in all XML dialects to mark up the natural human language that an element contains.
-# 
-# There is also a lang attribute (without namespace). If both of them are defined, the one with namespace is used and the one without is ignored.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:lang')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', '<language-tag>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlLang,
-# SVG supports the built-in XML **`xml:space`** attribute to handle whitespace characters inside elements. Child elements inside an element may also have an `xml:space` attribute that overrides the parent's one.
-# 
-# > **Note:** Instead of using the `xml:space` attribute, use the white-space CSS property.
-# 
-# This attribute influences how browsers parse text content and therefore changes the way the DOM is built. Therefore, changing this attribute's value through the DOM API may have no effect.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:space')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', 'default | preserve')]
-[ArgumentCompleter({
-    param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
-
-    $validSet = 'default','preserve'
-    if ($wordToComplete) {        
-        $toComplete = $wordToComplete -replace "^'" -replace "'$"
-        return @($validSet -like "$toComplete*" -replace '^', "'" -replace '$',"'")
-    } else {
-        return @($validSet -replace '^', "'" -replace '$',"'")
-    }
-})]
-[Reflection.AssemblyMetaData('SVG.Default value', 'default')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlSpace
+$In
 )
 
 
@@ -258,6 +222,10 @@ process {
         if ($on) {
             # put it into the splat.
             $writeSvgSplat.On = $on
+        }
+
+        if ($slot) {
+            $writeSvgSplat.Slot = $slot
         }
 
         . Write-SVG @writeSvgSplat
