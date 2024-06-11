c54f39305626cdf26df61899b84133bb0d51fc14
StartAutomating <StartAutomating@github.com>
2024-06-11 19:02:12 +0000

    feat: Forcing all MDN elements to have -ID ( Fixes #217 )
        Also, not transpiling generating functions

--- Commands/Standard/SVG.desc.ps1
+++ Commands/Standard/SVG.desc.ps1
@@ -1,5 +1,4 @@
-function SVG.desc {
-
+function SVG.desc  {
 <#
 .Synopsis
     Creates SVG desc elements
@@ -24,6 +23,10 @@ param(
 [Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 [Alias('InputObject','Text', 'InnerText', 'Contents')]
 $Content,
+# The element identifier.
+[Parameter(ValueFromPipelineByPropertyName)]
+[string]
+$Id,
 # A dictionary containing data.  This data will be embedded in data- attributes.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('DataAttribute','DataAttributes')]
@@ -94,6 +97,10 @@ process {
             }
         }
 
+        if ($paramCopy.Id) {
+            $attribute.Id = $paramCopy.Id
+        }
+
         # All commands will call Write-SVG.  Prepare a splat.
         $writeSvgSplat = @{
             ElementName = $elementName
@@ -138,12 +145,11 @@ process {
 
         if ($slot) {
             $writeSvgSplat.Slot = $slot
-        }
+        }        
 
         . Write-SVG @writeSvgSplat
     
 }
-
 
 } 
 
