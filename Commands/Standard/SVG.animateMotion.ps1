93fecf5eb0e991f32bf34949a62e146ec4fd21cc
StartAutomating <StartAutomating@github.com>
2024-06-11 18:44:22 +0000

    feat: Write-SVG -Slot and Regenerating from MDN ( Fixes #217 )

--- Commands/Standard/SVG.animateMotion.ps1
+++ Commands/Standard/SVG.animateMotion.ps1
@@ -8,26 +8,32 @@ function SVG.animateMotion {
     
     > **Note:** To reuse an existing path, it will be necessary to use an `mpath` element inside the `<animateMotion>` element instead of the `path` attribute.
 .Example
-    $path = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
+    param(
+    [string]$AnimationPath = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
+    )
+    
     SVG -viewBox "0 0 200 100" @(
-        SVG.path -d $path -Fill none -Stroke lightgrey
+        SVG.path -d $AnimationPath -Fill none -Stroke lightgrey
         SVG.circle -r 5 -Fill red (
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
         )
         SVG.rect -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
             SVG.animateTransform -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
         )
     )
 .Example
-    $path = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
+    param(
+    [string]$AnimationPath = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
+    )
+    
     SVG -viewBox "0 0 200 100" @(
-        SVG.path -d $path -Fill none -Stroke lightgrey
+        SVG.path -d $AnimationPath -Fill none -Stroke lightgrey
         SVG.circle -r 5 -Fill red (
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
         )
         SVG.rect -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $path
+            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
             SVG.animateTransform -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
         )
     )
@@ -55,7 +61,13 @@ $Data,
 # Each key or property name will be the name of the event
 # Each value will be the handler.
 [Parameter(ValueFromPipelineByPropertyName)]
+[PSObject]
 $On,
+# The slot attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Alias("SlotName")]
+[string]
+$Slot,
 # A dictionary of attributes.  This can set any attribute not exposed in other parameters.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('SVGAttributes','SVGAttribute')]
@@ -435,115 +447,7 @@ $Additive,
 })]
 [Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
 [PSObject]
-$Accumulate,
-# The **`id`** attribute assigns a unique name to an element.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','id')]
-[Reflection.AssemblyMetaData('SVG.Value', '<id>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Id,
-# The **`lang`** attribute specifies the primary language used in contents and attributes containing text content of particular elements.
-# 
-# There is also an xml:lang attribute (with namespace). If both of them are defined, the one with namespace is used and the one without is ignored.
-# 
-# In SVG 1.1 there was a `lang` attribute defined with a different meaning and only applying to glyph elements. That attribute specified a list of languages according to {{RFC(5646, "Tags for Identifying Languages (also known as BCP 47)")}}. The glyph was meant to be used if the `xml:lang` attribute exactly matched one of the languages given in the value of this parameter, or if the `xml:lang` attribute exactly equaled a prefix of one of the languages given in the value of this parameter such that the first tag character following the prefix was "-".
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','lang')]
-[Reflection.AssemblyMetaData('SVG.Value', '<language-tag>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Lang,
-# The **`tabindex`** attribute allows you to control whether an element is focusable and to define the relative order of the element for the purposes of sequential focus navigation.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','tabindex')]
-[Reflection.AssemblyMetaData('SVG.Value', 'valid integer')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Tabindex,
-# The **`xml:base`** attribute specifies a base IRI other than the base IRI of the document or external entity.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:base')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', '<iri>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlBase,
-# The **`xml:lang`** attribute specifies the primary language used in contents and attributes containing text content of particular elements.
-# 
-# It is a universal attribute allowed in all XML dialects to mark up the natural human language that an element contains.
-# 
-# There is also a lang attribute (without namespace). If both of them are defined, the one with namespace is used and the one without is ignored.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:lang')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', '<language-tag>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlLang,
-# SVG supports the built-in XML **`xml:space`** attribute to handle whitespace characters inside elements. Child elements inside an element may also have an `xml:space` attribute that overrides the parent's one.
-# 
-# > **Note:** Instead of using the `xml:space` attribute, use the white-space CSS property.
-# 
-# This attribute influences how browsers parse text content and therefore changes the way the DOM is built. Therefore, changing this attribute's value through the DOM API may have no effect.
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xml:space')]
-[Reflection.AssemblyMetaData('SVG.Deprecated',$true)]
-[Reflection.AssemblyMetaData('SVG.Value', 'default | preserve')]
-[ArgumentCompleter({
-    param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
-
-    $validSet = 'default','preserve'
-    if ($wordToComplete) {        
-        $toComplete = $wordToComplete -replace "^'" -replace "'$"
-        return @($validSet -like "$toComplete*" -replace '^', "'" -replace '$',"'")
-    } else {
-        return @($validSet -replace '^', "'" -replace '$',"'")
-    }
-})]
-[Reflection.AssemblyMetaData('SVG.Default value', 'default')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$XmlSpace,
-# « [SVG Attribute reference home](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute)
-# 
-# Assigns a class name or set of class names to an element. You may assign the same class name or names to any number of elements, however, multiple class names must be separated by whitespace characters.
-# 
-# An element's class name serves two key roles:
-# 
-# - As a style sheet selector, for when an author assigns style information to a set of elements.
-# - For general use by the browser.
-# 
-# You can use this class to style SVG content using CSS.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','class')]
-[Reflection.AssemblyMetaData('SVG.Categories', 'None')]
-[Reflection.AssemblyMetaData('SVG.Value', '<list-of-class-names>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
-[Reflection.AssemblyMetaData('SVG.Normative document', 'SVG 1.1 (2nd Edition): The class attribute')]
-[PSObject]
-$Class,
-# The **`style`** attribute allows to style an element using CSS declarations. It functions identically to [the `style` attribute in HTML](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/style).
-# 
-# You can use this attribute with any SVG element.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','style')]
-[Reflection.AssemblyMetaData('SVG.Value', '<style>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'False')]
-[PSObject]
-$Style
+$Accumulate
 )
 
 
@@ -614,6 +518,10 @@ process {
         if ($on) {
             # put it into the splat.
             $writeSvgSplat.On = $on
+        }
+
+        if ($slot) {
+            $writeSvgSplat.Slot = $slot
         }
 
         . Write-SVG @writeSvgSplat
