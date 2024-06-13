f6c527e10d6aa0c95778f4f726efd1a4eef47074
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:31 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.path.ps1
+++ Commands/Standard/SVG.path.ps1
@@ -5,20 +5,9 @@ function SVG.path  {
 .Description
     The **`<path>`** [SVG](https://developer.mozilla.org/en-US/docs/Web/SVG) element is the generic element to define a shape. All the basic shapes can be created with a path element.
 .Example
-    param(
-    [string]$AnimationPath = "M20,50 C20,-50 180,150 180,50 C180-50 20,150 20,50 z"
-    )
-    
-    SVG -viewBox "0 0 200 100" @(
-        SVG.path -d $AnimationPath -Fill none -Stroke lightgrey
-        SVG.circle -r 5 -Fill red (
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
-        )
-        SVG.rect -Width 2 -Height 2 -X -1 -Y -1 -Fill blue @(
-            SVG.animateMotion -Dur 10s -RepeatCount 'indefinite' -Path $AnimationPath
-            SVG.animateTransform -AttributeName transform -From "0 0 0"  -To "360 0 0" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
-        )
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./AnimateMotion.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.path
 .Link
