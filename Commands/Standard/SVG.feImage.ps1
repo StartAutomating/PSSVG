d7d353ecf2070d645c7137733f7249d1e39b70e6
StartAutomating <StartAutomating@github.com>
2024-06-13 04:11:59 +0000

    feat: MDN Refactor ( Fixes #218 )
        Adjusting parameter names.

--- Commands/Standard/SVG.feImage.ps1
+++ Commands/Standard/SVG.feImage.ps1
@@ -72,13 +72,26 @@ $Crossorigin,
 [Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
 [PSObject]
 $PreserveAspectRatio,
-# 
+# The XlinkHref attribute.
 [Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','xlink:href')]
-[Reflection.AssemblyMetaData('SVG.Value', '<iri>')]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
+[Reflection.AssemblyMetaData('SVG.AttributeName','XlinkHref')]
 [PSObject]
-$XlinkHref
+$XlinkHref,
+# The ClinkHref attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','ClinkHref')]
+[PSObject]
+$ClinkHref,
+# The SlinkHref attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','SlinkHref')]
+[PSObject]
+$SlinkHref,
+# The ElinkHref attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','ElinkHref')]
+[PSObject]
+$ElinkHref
 )
 
 
