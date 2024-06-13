7380a0876b0a6c6a784e938f77fc1de048216207
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:30 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.feConvolveMatrix.ps1
+++ Commands/Standard/SVG.feConvolveMatrix.ps1
@@ -39,44 +39,9 @@ function SVG.feConvolveMatrix  {
      3*200 + 2*220 + 1*240) / (9+8+7+6+5+4+3+2+1)
     ```
 .Example
-    param(
-    # The message to emboss
-    [string]
-    $Message = "Embossed",
-    
-    # The fill color of the text
-    [string]
-    $Fill = '#4488ff',
-    
-    # The css classes to apply to the image
-    [string[]]
-    $Class,
-    
-    # The font size of the text
-    [string]
-    $FontSize = "26em",
-    
-    # The embossing level
-    [int]
-    $Emboss = 5
-    )
-    svg @(
-        svg.filter -id embossed @(
-            svg.feConvolveMatrix -KernelMatrix "
-            $Emboss 0 0
-            0 0 0
-            0 0 -$($Emboss * -1)
-    "
-            svg.feMerge @(
-                svg.feMergeNode
-                svg.feMergeNode -In 'SourceGraphic'
-            )
-        )
-    
-        svg.text "
-    Embossed
-    " -TextAnchor middle -DominantBaseline middle -Fill $Fill -FontSize $FontSize -X 50% -Y 50% -Filter 'url(#embossed)'
-    ) -Class $class -ViewBox 0,0,300,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./Embossed.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.feConvolveMatrix
 .Link
