70e05ab1028714a52fc694904642502e75c3017d
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:31 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.symbol.ps1
+++ Commands/Standard/SVG.symbol.ps1
@@ -7,39 +7,13 @@ function SVG.symbol  {
     
     The use of `<symbol>` elements for graphics that are used multiple times in the same document adds structure and semantics. Documents that are rich in structure may be rendered graphically, as speech, or as Braille, and thus promote accessibility.
 .Example
-    svg -ViewBox 100,100 -Content @(
-        svg.symbol -Id psChevron -Content @(
-            svg.polygon -Points (@(
-                "40,20"
-                "45,20"
-                "60,50"
-                "35,80"
-                "32.5,80"
-                "55,50"
-            ) -join ' ')
-        ) -ViewBox 100, 100
-    
-        svg.use -Href '#psChevron' -Fill '#4488ff'
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./PowerShellChevron.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -viewBox 300, 100 -Content @(
-        svg.symbol -Id psChevron -Content @(
-            svg.polygon -Points (@(
-                "40,20"
-                "45,20"
-                "60,50"
-                "35,80"
-                "32.5,80"
-                "55,50"
-            ) -join ' ')
-        ) -ViewBox 100, 100
-        svg.use -Href '#psChevron' -Fill '#4488ff' -X -7.5%
-        svg.text @(
-            svg.tspan -Content 'Start' -LetterSpacing .15em -AlignmentBaseline 'middle'
-            svg.tspan -Content 'Automating' -LetterSpacing .2em -AlignmentBaseline 'middle' -Dx 0.5em
-        ) -FontFamily 'monospace' -AlignmentBaseline 'middle' -X 27.5% -Y 50% -Fill '#4488ff'
-        # svg.text -Content 'Automating' -FontFamily 'monospace' -AlignmentBaseline 'middle' -X 45% -Y 55% -Fill '#4488ff' -LetterSpacing .1em
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./StartAutomating.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.symbol
 .Link
