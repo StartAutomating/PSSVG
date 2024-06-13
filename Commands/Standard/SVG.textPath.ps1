3398f52d41ebdabdbbe72f30b3b39bf0d358d87b
StartAutomating <StartAutomating@github.com>
2024-06-13 04:34:31 +0000

    feat: MDN Refactor ( Fixes #218 )
        Better handling categories and AppliesTo

--- Commands/Standard/SVG.textPath.ps1
+++ Commands/Standard/SVG.textPath.ps1
@@ -160,48 +160,96 @@ $Spacing,
 [Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
 [PSObject]
 $StartOffset,
-# The width of the space into which the text will render.
+# The TextLength attribute.
 [Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','textLength')]
-[Reflection.AssemblyMetaData('SVG.Value', '
-        {{cssxref("length-percentage")}} |
-        {{cssxref("number")}}
-      ')]
-[ValidatePattern('(?>|\d+)')]
-[ArgumentCompleter({
-    param ( $commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters )    
-
-    $validSet = '<length-percentage>','<number>'
-    if ($wordToComplete) {        
-        $toComplete = $wordToComplete -replace "^'" -replace "'$"
-        return @($validSet -like "$toComplete*" -replace '^', "'" -replace '$',"'")
-    } else {
-        return @($validSet -replace '^', "'" -replace '$',"'")
-    }
-})]
-[Reflection.AssemblyMetaData('SVG.Animatable', 'True')]
+[Reflection.AssemblyMetaData('SVG.AttributeName','TextLength')]
 [PSObject]
 $TextLength,
-# The CextLength attribute.
-[Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','CextLength')]
-[PSObject]
-$CextLength,
 # The SextLength attribute.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','SextLength')]
 [PSObject]
 $SextLength,
-# The EextLength attribute.
+# The RextLength attribute.
 [Parameter(ValueFromPipelineByPropertyName)]
-[Reflection.AssemblyMetaData('SVG.AttributeName','EextLength')]
+[Reflection.AssemblyMetaData('SVG.AttributeName','RextLength')]
 [PSObject]
-$EextLength,
+$RextLength,
+# The IextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','IextLength')]
+[PSObject]
+$IextLength,
+# The CextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','CextLength')]
+[PSObject]
+$CextLength,
+# The LextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','LextLength')]
+[PSObject]
+$LextLength,
+# The FextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','FextLength')]
+[PSObject]
+$FextLength,
+# The BextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','BextLength')]
+[PSObject]
+$BextLength,
+# The MextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','MextLength')]
+[PSObject]
+$MextLength,
+# The VextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','VextLength')]
+[PSObject]
+$VextLength,
+# The DextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','DextLength')]
+[PSObject]
+$DextLength,
+# The OextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','OextLength')]
+[PSObject]
+$OextLength,
+# The WextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','WextLength')]
+[PSObject]
+$WextLength,
+# The PextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','PextLength')]
+[PSObject]
+$PextLength,
+# The UextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','UextLength')]
+[PSObject]
+$UextLength,
+# The AextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','AextLength')]
+[PSObject]
+$AextLength,
 # The XextLength attribute.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Reflection.AssemblyMetaData('SVG.AttributeName','XextLength')]
 [PSObject]
-$XextLength
+$XextLength,
+# The EextLength attribute.
+[Parameter(ValueFromPipelineByPropertyName)]
+[Reflection.AssemblyMetaData('SVG.AttributeName','EextLength')]
+[PSObject]
+$EextLength
 )
 
 
