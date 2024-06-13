6643c14b378bd7a8189c6b78d785f9242780e962
James Brundage <@github.com>
2024-06-12 21:38:32 -0700

    feat: MDN Refactor ( Fixes #218 )
    Parameter name fixes

--- Build/PSSVG.Build.ps.ps1
+++ Build/PSSVG.Build.ps.ps1
@@ -941,7 +941,7 @@ If nothing was provided, each output will be decorated with it's ElementName.
     foreach ($potentiallyMissing in $checkForTheseParameters) {    
         if (-not $parameters[$potentiallyMissing]) {
             if ($attributeFileData[$potentiallyMissing].IsDeprecated) { continue }
-            $potentiallyMissing = $potentiallyMissing.Substring(0,1).ToUpper() + $paramName.Substring(1)
+            $potentiallyMissing = $potentiallyMissing.Substring(0,1).ToUpper() + $potentiallyMissing.Substring(1)
             $potentiallyMissing = $potentiallyMissing -replace '\W'
             $parameters[$potentiallyMissing] = @(
                 "# The $potentiallyMissing attribute."
