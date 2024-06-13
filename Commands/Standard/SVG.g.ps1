936dfb3bfbaf575db804727beb6bde4d48734192
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:31 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.g.ps1
+++ Commands/Standard/SVG.g.ps1
@@ -7,39 +7,9 @@ function SVG.g  {
     
     Transformations applied to the `<g>` element are performed on its child elements, and its attributes are inherited by its children. It can also group multiple elements to be referenced later with the `use` element.
 .Example
-    param(
-    # The Message to display
-    [string]
-    $Message = "Fading in",
-    
-    # The fill color of the text
-    [string]
-    $Fill = '#4488ff',
-    
-    # One or more CSS classes to apply to the text
-    [string[]]
-    $Class,
-    
-    # The font size of the text
-    [string]
-    $FontSize = "26em",
-    
-    # The duration of the animation.
-    [Timespan]
-    $Duration = "00:00:02",
-    
-    [string]
-    $RepeatCount = 'indefinite'
-    )
-    
-    svg -ViewBox 0,0,1920,1080 -Content (
-        svg.g -Content @(
-            svg.text -Y "50%" -X "50%" -DominantBaseline middle -TextAnchor middle -Text $Message -Fill $fill -Class $Class -fontSize $FontSize
-    
-            # If you only want to fade in once, remove the -RepeatCount
-            svg.animate -Values '0;1' -AttributeName opacity -Begin '0s' -End "$($Duration.TotalSeconds)" -Dur "$($duration.TotalSeconds)s" -RepeatCount $RepeatCount
-        )
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./FadeIn.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.g
 .Link
