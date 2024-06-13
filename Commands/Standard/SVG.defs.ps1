8c56d01208b1053924fe572d11d17055d99037f4
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:30 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.defs.ps1
+++ Commands/Standard/SVG.defs.ps1
@@ -7,326 +7,61 @@ function SVG.defs  {
     
     Graphical objects can be referenced from anywhere, however, defining these objects inside of a `<defs>` element promotes understandability of the SVG content and is beneficial to the overall accessibility of the document.
 .Example
-    param(
-    [string]
-    $Message = "Hello World",
-    
-    [string]
-    $GoogleFont = "Roboto",
-    
-    [string]
-    $Fill = '#4488ff',
-    
-    [string[]]
-    $Class,
-    
-    [string]
-    $FontSize = "24em",
-    
-    [double]
-    $Width = 1920,
-    
-    [double]
-    $Height = 1080
-    )
-    
-    svg @(
-        svg.defs @(
-            SVG.GoogleFont -FontName $GoogleFont
-        )
-        svg.text -X 50% -Y 50% -Fontsize $FontSize $Message -DominantBaseline middle -TextAnchor middle -Fill $Fill -Class $Class -FontFamily "'$GoogleFont', sans-serif"
-    ) -ViewBox 0,0, 1920, 1080
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./HelloWorld.PSSVG.ps1
+    Pop-Location
 .Example
-    param(
-    # The scale of the pattern
-    [double]
-    $Scale = 0.75
-    )
-    SVG -Content @(
-        SVG.defs -Content @(
-    
-            SVG.pattern -id 'HexagonPattern' -patternUnits 'userSpaceOnUse' -width '174' -height '200' -patternTransform "scale($Scale)" -Content @(
-                SVG.Hexagon -CenterX 87 -CenterY 100 -Radius 100 -Fill transparent -Stroke '#4488ff' -Comment "A hexagon with a" -Class 'foreground-fill'
-            ) -Comment "A faint hexagon Pattern, rendered 4x at a base scale of 174 by 200"
-        )
-        $hugeSize = 20000
-    
-        SVG.rect -width "$hugeSize%" -height "$hugeSize%" -fill 'url(#HexagonPattern)'
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./HexagonPattern.PSSVG.ps1
+    Pop-Location
 .Example
-    param(
-    # The colors in the gradient
-    [string[]]
-    $Color = @('gold','red'),
-    
-    # The offsets in the gradient
-    [double[]]
-    $Offset = @(10,95)
-    )
-    svg -Content @(
-        svg.defs @(
-            svg.LinearGradient -Id myGradient -Content @(
-                for($n = 0; $n -lt $color.Length; $n++) {
-                    svg.stop -Offset "$($Offset[$n])%" -StopColor $color[$n]
-                }
-            )
-        )
-        svg.circle -Fill 'url(#myGradient)' -Cx 50 -Cy 50 -R 35
-    ) -viewbox 0,0,100,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./LinearGradient.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -Content @(
-        svg.defs @(
-            svg.LinearGradient -Id myGradient -Content @(
-                svg.stop -Stopcolor gold @(
-                    svg.animate -AttributeName offset -Values '.1;.99;.1' -Dur 5s -RepeatCount indefinite
-                )
-                svg.stop -Stopcolor red @(
-                    svg.animate -AttributeName offset -Values '100;0;100' -Dur 5s -RepeatCount indefinite
-                )
-            )
-        )
-        svg.rect -Fill 'url(#myGradient)' -x 0 -Y 0 -Width 100 -Height 100
-    ) -ViewBox '0 0 100 100'
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./LinearGradientAnimated.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -Content @(
-        svg.defs @(
-            svg.LinearGradient -Id myGradient -Content @(
-                svg.stop -Offset '10%' -Stopcolor transparent
-                svg.stop -Offset '95%' -Stopcolor '#4488ff'
-                svg.animate -AttributeName y1 -From 0 -To 1 -Id animateY1 -Fill freeze -Dur '3s'
-                svg.animate -AttributeName y2 -Dur "3s" -From 1 -to 0 -Id 'animateY2' -Fill freeze -Begin 'animateY1.end'
-                svg.animate -AttributeName x1 -Values '1;0' -Dur '3s' -Begin 'animateY2.end' -Fill freeze -Id animateX1
-                svg.animate -AttributeName x2 -Values '0;1' -Dur '3s' -Begin 'animateX1.end' -Fill freeze
-            ) -X1 100% -X2 0 -Y1 0% -Y2 100%
-    
-    
-        )
-        svg.rect -Fill 'url(#myGradient)' -Width 100 -Height 100
-    ) -viewbox 0,0,100,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./MovingGradient.PSSVG.ps1
+    Pop-Location
 .Example
-    svg @(
-        svg.defs @(
-            svg.pattern -Id 'SimplePattern' -Width .1 -Height .1 -Content @(
-                svg.circle -Cx 2.5 -Cy 2.5 -R .5 -Fill '#4488ff'
-                svg.line -X1 0 -x2 5 -y1 2.5 -Y2 2.5 -Stroke '#4488ff' -StrokeWidth .1
-                svg.line -Y1 0 -Y2 5 -X1 2.5 -X2 2.5 -Stroke '#4488ff' -StrokeWidth .1
-            )
-        )
-        svg.rect -Fill 'url(#SimplePattern)' -Width 50 -Height 50 -Opacity .3
-    ) -ViewBox 0,0,50,50
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./Pattern.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -ViewBox 0, 0, 250, 200 -Content @(
-        svg.defs (
-            svg.pattern -id star -ViewBox 0,0, 10, 10 -Width 10% -Height 10% @(
-                svg.polygon -Points "0,0", "2,5", "0,10", "5,8", "10,10","8,5", "10,0", "5,2" @(
-                    svg.animateTransform -AttributeName transform -From "0 5 5"  -To "360 5 5" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
-                ) -Fill '#4488ff'
-            )
-        )
-        svg.circle -cx 50 -cy 100 -r 50 -Fill 'url(#star)'
-        svg.circle -cx 180 -cy 100 -r 50 -Fill 'none' -StrokeWidth 20 -Stroke 'url(#star)' -Content @(
-            svg.animateTransform -AttributeName transform -From "0 180 100"  -To "360 180 100" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
-        )
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./PatternAnimation.PSSVG.ps1
+    Pop-Location
 .Example
-    param(
-    [timespan]
-    $Duration = '00:00:05'
-    )
-    svg -ViewBox 0, 0, 100, 100 -Content @(
-        svg.defs @(
-            svg.pattern -id star -ViewBox 0,0, 10, 10 -Width 10% -Height 10% @(
-                svg.polygon -Points "0,0", "2,5", "0,10", "5,8", "10,10","8,5", "10,0", "5,2" @(
-                    svg.animateTransform -AttributeName transform -From "0 5 5"  -To "360 5 5" -dur "$($Duration.TotalSeconds)s" -RepeatCount indefinite -AttributeType xml -type rotate
-                ) -Fill white
-            )
-            svg.mask (
-                svg.circle -Fill 'url(#star)' -r 50 -cx 50 -cy 50
-            ) -Id myMask
-            svg.radialGradient @(
-                svg.stop -Offset '25%' -StopColor 'red'
-                svg.stop -Offset '50%' -StopColor 'green'
-                svg.stop -Offset '75%' -StopColor 'blue'
-            ) -id myGradient
-        )
-        svg.circle -cx 50 -cy 50 -r 50 -Fill 'url(#myGradient)' -Mask 'url(#myMask)'
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./PatternMask.PSSVG.ps1
+    Pop-Location
 .Example
-    param(
-    # The colors in the gradient
-    [string[]]
-    $Color = @('#4488ff','#224488'),
-    
-    # The offsets in the gradient
-    [double[]]
-    $Offset = @(10,95)
-    )
-    
-    
-    svg -Content @(
-        svg.defs @(
-            svg.radialGradient -Id myGradient -Content @(
-                for($n = 0; $n -lt $color.Length; $n++) {
-                    svg.stop -Offset "$($Offset[$n])%" -StopColor $color[$n]
-                }
-            )
-        )
-        svg.circle -Fill 'url(#myGradient)' -Cx 50 -Cy 50 -R 35
-    ) -ViewBox 0,0,100,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./RadialGradient.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -Content @(
-        svg.defs @(
-            svg.radialGradient -Id myGradient -Content @(
-                svg.stop -Offset '1%' -Stopcolor gold @(
-                    svg.animate -AttributeName offset -Values '.1;.99;.1' -Dur 5s -RepeatCount indefinite
-                )
-                svg.stop -Offset '100%' -Stopcolor red
-            )
-        )
-        svg.circle -Fill 'url(#myGradient)' -Cx 50 -Cy 50 -R 35
-    ) -ViewBox '0 0 100 100'
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./RadialGradientAnimated.PSSVG.ps1
+    Pop-Location
 .Example
-    foreach ($n in 5..12) {
-    
-    
-    svg -ViewBox 2,2 @(
-        svg.Star -PointCount $n  -Fill 'transparent' -Stroke '#4488ff' -StrokeWidth 0.01
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./Stars.PSSVG.ps1
+    Pop-Location
 .Example
-    SVG -ViewBox 1.986,1 -Content @(
-        $g = (1.986 * .4) / 12
-        $e = (7/13)/10
-    
-        SVG.defs @(
-            SVG.Star -PointCount 5 -Radius (1/13 * .4) -Fill white -CenterX 0 -CenterY 0 -Rotate 180 -Id Star -Comment "Each Star has a radius of 2/5ths a Bar."
-        )
-    
-        SVG.title "American Flag"
-    
-        SVG.rect -Width 200% -Height 200% -x -50% -y -50% -Fill black
-    
-        1..13 |
-            SVG.rect -Id {"bar$_"} -Fill {
-                @("#FFFFFF", "#B22234")[$_ % 2]
-            } -Width 100% -Height "$((1/13) * 100)%" -Y { "$((($_ -1)/13 * 100))%" } -Comment "Each Bar is 1/13th the height"
-    
-    
-        SVG.rect -Fill "#3C3B6E" -Width 40% -Height "$((7/13 * 100))%" -X 0% -Y 0% -Id 'canton' -Comment "The Canton is 40% of the width and 7/13ths of the height"
-    
-        # Five rows of 6 stars
-        1..30 |
-            SVG.use -Id { "star$($_)" } -Href "#Star" -Comment "Five Rows of Six Stars" -Transform {
-                $g = (1.986 * .4) / 12
-                $e = (7/13)/10
-                "translate($(
-                    $g + ($g * 2 * ((($_ -1) % 6)))
-                ) $(
-                    $e + (
-                        $e * 2 * (([Math]::Floor(($_ - 1)/ 6)))
-                    )
-                ))"
-            }
-    
-        # Then interleaved with 4 rows of 5 stars
-        1..20 |
-            SVG.use -Id { "star$($_ + 30)" } -Href "#Star" -Width ($g/2) -Comment "Four Rows of Five Stars" -Children @(
-                # SVG.animateTransform -Type 'translate' -From $($g/2) -To $($g/2) -RepeatCount 'indefinite' -Dur 1s -AttributeName transform
-                # SVG.animateTransform -Type 'scale' -Values '.75;1.25;.75' -RepeatCount 'indefinite' -Dur ((60/128) * 2)s  -AttributeName transform -Additive 'sum'
-            ) -Transform {
-                $g = (1.986 * .4) / 12
-                $e = (7/13)/10
-                "translate($(
-                    ($g * 2) + ($g * 2 * ((($_ -1) % 5)))
-                ) $(
-                    ($e * 2) + (
-                        $e * 2 * (([Math]::Floor(($_ - 1)/ 5)))
-                    )
-                ))"
-            }
-    
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./US-Flag.PSSVG.ps1
+    Pop-Location
 .Example
-    param(
-    # The smaller Star Size (as a ratio)
-    [Alias('SmallerStarSize')]
-    [double]
-    $StarSizeSmall = .9,
-    # The larger Star Size (as a ratio)
-    [Alias('StarSizeBig','LargerStarSize')]
-    [double]
-    $StarSizeLarge = 1.1,
-    # The duration of the animation, in seconds.
-    # By default, two beats at 128 beats per minute.
-    [Alias('Interval')]
-    [double]
-    $Duration = $((60/128) * 2)
-    )
-    
-    
-    SVG -ViewBox 1.986,1 -Content @(
-        $g = (1.986 * .4) / 12
-        $e = (7/13)/10
-    
-        SVG.defs @(
-            SVG.Star -PointCount 5 -Radius (1/13 * .4) -Fill white -CenterX 0 -CenterY 0 -Rotate 180 -Id Star -Comment "Each Star has a radius of 2/5ths a Bar."
-        )
-    
-        SVG.title "American Flag"
-    
-        SVG.rect -Width 200% -Height 200% -x -50% -y -50% -Fill black
-    
-        1..13 |
-            SVG.rect -Id {"bar$_"} -Fill {
-                @("#FFFFFF", "#B22234")[$_ % 2]
-            } -Width 100% -Height "$((1/13) * 100)%" -Y { "$((($_ -1)/13 * 100))%" } -Comment "Each Bar is 1/13th the height"
-    
-    
-        SVG.rect -Fill "#3C3B6E" -Width 40% -Height "$((7/13 * 100))%" -X 0% -Y 0% -Id 'canton' -Comment "The Canton is 40% of the width and 7/13ths of the height"
-    
-    
-        # Five rows of 6 stars
-        1..30 |
-            SVG.use -Id { "star$($_)" } -Href "#Star" -Comment "Five Rows of Six Stars" -Transform {
-                $g = (1.986 * .4) / 12
-                $e = (7/13)/10
-                "translate($(
-                    $g + ($g * 2 * ((($_ -1) % 6)))
-                ) $(
-                    $e + (
-                        $e * 2 * (([Math]::Floor(($_ - 1)/ 6)))
-                    )
-                ))"
-            } -Children @(
-                SVG.animateTransform -Type 'scale' -Values "$StarSizeLarge;$StarSizeSmall;$StarSizeLarge" -RepeatCount 'indefinite' -Dur $Duration  -AttributeName transform -Additive 'sum'
-            )
-    
-        # Then interleaved with 4 rows of 5 stars
-        1..20 |
-            SVG.use -Id { "star$($_ + 30)" } -Href "#Star" -Width ($g/2) -Comment "Four Rows of Five Stars" -Children @(
-                SVG.animateTransform -Type 'scale' -Values "$StarSizeSmall;$StarSizeLarge;$StarSizeSmall" -RepeatCount 'indefinite' -Dur $Duration  -AttributeName transform -Additive 'sum'
-            ) -Transform {
-                $g = (1.986 * .4) / 12
-                $e = (7/13)/10
-                "translate($(
-                    ($g * 2) + ($g * 2 * ((($_ -1) % 5)))
-                ) $(
-                    ($e * 2) + (
-                        $e * 2 * (([Math]::Floor(($_ - 1)/ 5)))
-                    )
-                ))"
-            }
-    
-    )
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./US-FlagAnimated.PSSVG.ps1
+    Pop-Location
 .Example
-    svg -Content @(
-        svg.defs @(
-            svg.LinearGradient -Id myGradient -Content @(
-                svg.stop -Offset '10%' -Stopcolor gold
-                svg.stop -Offset '95%' -Stopcolor red
-            ) -X1 0 -X2 0 -Y1 0% -Y2 100%
-        )
-        svg.rect -Fill 'url(#myGradient)' -Width 100 -Height 100
-    ) -viewbox 0,0,100,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./VerticalLinearGradient.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.defs
 .Link
