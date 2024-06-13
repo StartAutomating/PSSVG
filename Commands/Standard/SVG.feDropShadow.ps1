fd4ec9c3c197a3283c970a5d89e0ae1074806499
StartAutomating <StartAutomating@github.com>
2024-06-13 04:55:30 +0000

    feat: MDN Refactor File Size Reductions ( Fixes #218, Fixes #219 )
        Making core examples references (quoting reference, not running reference)

--- Commands/Standard/SVG.feDropShadow.ps1
+++ Commands/Standard/SVG.feDropShadow.ps1
@@ -7,39 +7,9 @@ function SVG.feDropShadow  {
     
     > **Note:** The drop shadow color and opacity can be changed by using the `flood-color` and `flood-opacity` presentation attributes.
 .Example
-    param(
-    [string]
-    $Fill = '#4488ff',
-    
-    [string[]]
-    $Class,
-    
-    [timespan]
-    $Duration = '00:00:02.5',
-    
-    [double[]]
-    $ShadowX = @(0.5, -.5, 0.5),
-    
-    [double[]]
-    $ShadowY = @(0.75, -.75, 0.75)
-    )
-    
-    svg @(
-        svg.filter -id dropShadow @(
-            svg.feDropShadow -dx $ShadowX[0] -dy $ShadowY[0] -StdDeviation 0 @(
-                svg.animate -AttributeName dx -Values $($ShadowX -join ';') -Dur $Duration -RepeatCount 'indefinite'
-                svg.animate -AttributeName dY -Values $($Shadowy -join ';') -Dur $Duration -RepeatCount 'indefinite'
-            )
-            svg.feMerge @(
-                svg.feMergeNode
-                svg.feMergeNode -In 'SourceGraphic'
-            )
-        )
-    
-        svg.text "
-    Moving Shadows
-    " -TextAnchor middle -DominantBaseline middle -Fill $fill -Class $Class -FontSize 16 -X 50% -Y 50% -Filter 'url(#dropShadow)'
-    ) -ViewBox 0,0,300,100
+    Get-Module PSSVG | Split-Path | Join-Path -ChildPath Examples | Push-Location
+    ./MovingShadows.PSSVG.ps1
+    Pop-Location
 .Link
     https://pssvg.start-automating.com/SVG.feDropShadow
 .Link
