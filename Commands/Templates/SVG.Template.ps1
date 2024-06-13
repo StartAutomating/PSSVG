bb3e989e9ef3f24ad1366a12e20094d9ac9a4bfa
StartAutomating <StartAutomating@github.com>
2024-06-13 05:48:00 +0000

    feat: SVG.Template ( Fixes #215 )

---
+++ Commands/Templates/SVG.Template.ps1
@@ -0,0 +1,81 @@
+[ValidatePattern('SVG\.Template')]
+param()
+function SVG.Template {
+    <#
+    
+    .SYNOPSIS    
+        Creates a template element.    
+    .DESCRIPTION    
+        Creates a template element containing SVG.    
+        While templates are not _technically_ SVG elements, they're very useful when combined with SVGs.    
+    
+    #>
+        
+    param(
+    
+    )
+    dynamicParam {
+    $baseCommand = 
+        if (-not $script:SVGsvg) {
+            $script:SVGsvg = 
+                $executionContext.SessionState.InvokeCommand.GetCommand('SVG.svg','Function')
+            $script:SVGsvg
+        } else {
+            $script:SVGsvg
+        }
+    $IncludeParameter = @()
+    $ExcludeParameter = @()
+
+
+    $DynamicParameters = [Management.Automation.RuntimeDefinedParameterDictionary]::new()            
+    :nextInputParameter foreach ($paramName in ([Management.Automation.CommandMetaData]$baseCommand).Parameters.Keys) {
+        if ($ExcludeParameter) {
+            foreach ($exclude in $ExcludeParameter) {
+                if ($paramName -like $exclude) { continue nextInputParameter}
+            }
+        }
+        if ($IncludeParameter) {
+            $shouldInclude = 
+                foreach ($include in $IncludeParameter) {
+                    if ($paramName -like $include) { $true;break}
+                }
+            if (-not $shouldInclude) { continue nextInputParameter }
+        }
+        
+        $DynamicParameters.Add($paramName, [Management.Automation.RuntimeDefinedParameter]::new(
+            $baseCommand.Parameters[$paramName].Name,
+            $baseCommand.Parameters[$paramName].ParameterType,
+            $baseCommand.Parameters[$paramName].Attributes
+        ))
+    }
+    $DynamicParameters
+
+    }
+    end {
+
+        # Anything piped in is content to template.
+        $ContentTotemplate = @($input)
+        if (-not $ContentTotemplate) {
+            # If no content is piped in, check the parameter.
+            $ContentTotemplate = $PSBoundParameters['Content']
+        }
+        # Return if there is nothing to template.
+        if (-not $ContentTotemplate) { return }
+
+         # Create a splat of parameters to pass to the base command.
+         $svgSplat = [Ordered]@{} + $PSBoundParameters
+         $svgCommand = $baseCommand
+         # Remove any of my parameters that are not in the base command.
+         foreach ($parameterName in @($svgSplat.Keys)) {
+             if (-not $svgCommand.Parameters[$parameterName]) {
+                 $svgSplat.Remove($parameterName)
+             }
+         }
+         # and remove `Content` for good measure.
+         $svgSplat.Remove('Content')
+
+         Write-SVG -ElementName template -Content @( & $baseCommand @svgSplat)
+    
+    }
+}
+
