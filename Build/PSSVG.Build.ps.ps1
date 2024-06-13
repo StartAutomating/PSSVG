a21edcf70fe91936929eeee9fcf2d55c7954f013
James Brundage <@github.com>
2024-06-13 10:10:04 -0700

    feat: MDN Refactor - Adding SVG.Attribute.Types.ps1xml ( Fixes #218 )
    Removing empty note properties

--- Build/PSSVG.Build.ps.ps1
+++ Build/PSSVG.Build.ps.ps1
@@ -223,7 +223,7 @@ switch -regex ($svgAttributesByCategory) {
 @(foreach ($kvp in $attributeFileData.GetEnumerator()) {
     $noteProps = [Ordered]@{} + $kvp.Value
     foreach ($key in @($noteProps.Keys)) {
-        if ($null -eq $noteProps[$key]) {
+        if ($null -eq $noteProps[$key] -or $noteProps[$key] -match '^\s{0,}$') {
             $noteProps.Remove($key)
         }
     }
