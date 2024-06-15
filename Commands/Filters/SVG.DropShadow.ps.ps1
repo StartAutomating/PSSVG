fcda1476e9bb64beb204ec21b6c7c3cfe6b03434
James Brundage <@github.com>
2024-06-15 11:08:13 -0700

    chore: Moving SVG.DropShadow into /Filters ( Fixes #221, Fixes #157 )
    And adding build condition

--- Commands/Filters/SVG.DropShadow.ps.ps1
+++ Commands/Filters/SVG.DropShadow.ps.ps1
@@ -1,3 +1,5 @@
+[ValidatePattern('SVG\.DropShadow')]
+param()
 function SVG.DropShadow
 {
     <#
@@ -27,10 +29,10 @@ function SVG.DropShadow
 
     process {
         $DropShadow = @(            
-            =<svg.feDropShadow> -dx $DistanceX -dy $DistanceY -StdDeviation $StandardDeviation
-            =<svg.feMerge> @(
-                =<svg.feMergeNode>
-                =<svg.feMergeNode> -In 'SourceGraphic'
+            svg.feDropShadow -dx $DistanceX -dy $DistanceY -StdDeviation $StandardDeviation
+            svg.feMerge @(
+                svg.feMergeNode
+                svg.feMergeNode -In 'SourceGraphic'
             )            
         )
         if ($PSBoundParameters['Content']) {
@@ -43,6 +45,7 @@ function SVG.DropShadow
         }
         $null = $PSBoundParameters.Remove('DistanceX')
         $null = $PSBoundParameters.Remove('DistanceY')
+        $null = $PSBoundParameters.Remove('StandardDeviation')
         SVG.filter @PSBoundParameters
     }
 }
