55b20e59467c1ec9949c93b9cb7a814284168537
StartAutomating <StartAutomating@github.com>
2024-06-16 04:35:40 +0000

    feat: SVG.HueRotate ( Fixes #222 )
        Improving piping and inner docs

--- Commands/Filters/SVG.HueRotate.ps1
+++ Commands/Filters/SVG.HueRotate.ps1
@@ -60,20 +60,62 @@ function SVG.HueRotate {
     $DynamicParameters
 
     }
+        begin {
+        $animationElements = 'animate','set','animateTransform','animateMotion'
+    
+    }
         process {
-        $HueRotateFilter = @(            
-            SVG.feColorMatrix -Type 'hueRotate' -Values $HueRotate -In 'SourceGraphic'
-        )
-        if ($PSBoundParameters['Content']) {
-            $PSBoundParameters['Content'] = $HueRotateFilter + $PSBoundParameters['Content']
+        # Create the splat for the filter and filter entry
+        $feSplat = [Ordered]@{type='hueRotate';Values=$HueRotate}
+        $filterSplat = [Ordered]@{} + $PSBoundParameters
+
+        # Check if we have any content
+        $content = $PSBoundParameters['Content']
+        if ($content.LocalName) {
+            # If it's not a filter
+            if ($content.LocalName -ne 'filter') {
+                # Set the input to SourceGraphic
+                $feSplat['In'] = 'SourceGraphic'
+                # If it's an animation element, set the content to the animation element
+                if ($content.LocalName -in $animationElements) {
+                    $feSplat['Content'] = $content
+                }
+                
+            }
         } else {
-            $PSBoundParameters['Content'] = $HueRotateFilter
+            # If there was no content, set the input to SourceGraphic
+            $feSplat['In'] = 'SourceGraphic'
         }
-        if (-not $PSBoundParameters['ID']) {
-            $PSBoundParameters['ID'] = 'hueRotate'
-        }        
-        $null = $PSBoundParameters.Remove('HueRotate')
-        SVG.filter @PSBoundParameters
+
+        # Create the saturation filter (using [feColorMatrix](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix))
+        $newFilter = SVG.feColorMatrix @feSplat
+        
+        $filterSplat['Content'] =
+            # If the content is a filter, add the filter to the content
+            if ($content.LocalName -eq 'Filter') {
+                @($content.childNodes) + $newFilter
+            } else {
+                $newFilter
+            }
+        
+        # If there's no ID, set it to $defaultId
+        if (-not $filterSplat['ID']) {
+            $filterSplat['ID'] = $defaultId
+        }
+        # Remove parameters from the splat that don't apply to filter.
+        $null = $filterSplat.Remove('saturation')
+
+        if ($content -and 
+            $content.LocalName -ne 'filter' -and 
+            $content.LocalName -notin $animationElements) {
+            SVG.defs @(SVG.filter @filterSplat)
+            if ($content.setAttribute) {
+                $content.setAttribute('style',"filter:url('#$($filterSplat['ID'])')")
+            }
+            $content
+        } else {
+            SVG.filter @filterSplat
+        }
     
     }
 }
