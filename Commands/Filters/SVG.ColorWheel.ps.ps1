396c7842e429c00ba22087ee3d5e16f9f5e9bf61
James Brundage <@github.com>
2024-06-15 23:39:58 -0700

    feat: SVG.ColorWheel ( Fixes #223 )
    Improving piping

--- Commands/Filters/SVG.ColorWheel.ps.ps1
+++ Commands/Filters/SVG.ColorWheel.ps.ps1
@@ -33,23 +33,67 @@ function SVG.ColorWheel
     [timespan]
     $Duration = '00:00:04.2'
     )
+
+    begin {
+        $animationElements = 'animate','set','animateTransform','animateMotion'
+        $defaultId = 'colorWheel'
+        $notFilterParameters = 'WheelStart','WheelEnd','Duration'
+    }
     process {
-        $HueRotateFilter = @(            
-            SVG.feColorMatrix -Type 'hueRotate' -In 'SourceGraphic' @(
-                SVG.animate -AttributeName 'values' -Values "$WheelStart; $WheelEnd" -Dur '10s' -RepeatCount 'indefinite'
-            )
-        )
-        if ($PSBoundParameters['Content']) {
-            $PSBoundParameters['Content'] = $HueRotateFilter + $PSBoundParameters['Content']
-        } else {
-            $PSBoundParameters['Content'] = $HueRotateFilter
+        # Create the splat for the filter and filter entry
+        $feSplat = [Ordered]@{
+            type='hueRotate'
+            Content = @(SVG.animate -AttributeName 'values' -Values "$WheelStart; $WheelEnd" -Dur $Duration -RepeatCount 'indefinite')
         }
-        if (-not $PSBoundParameters['ID']) {
-            $PSBoundParameters['ID'] = 'colorWheel'
-        }        
-        $null = $PSBoundParameters.Remove('WheelStart')
-        $null = $PSBoundParameters.Remove('WheelEnd')
-        $null = $PSBoundParameters.Remove('Duration')
-        SVG.filter @PSBoundParameters
+        $filterSplat = [Ordered]@{} + $PSBoundParameters
+
+        # Check if we have any content
+        $content = $PSBoundParameters['Content']
+        if ($content.LocalName) {
+            # If it's not a filter
+            if ($content.LocalName -ne 'filter') {
+                # Set the input to SourceGraphic
+                $feSplat['In'] = 'SourceGraphic'
+                # If it's an animation element, set the content to the animation element
+                if ($content.LocalName -in $animationElements) {
+                    $feSplat['Content'] += $content
+                }                
+            }
+        } else {
+            # If there was no content, set the input to SourceGraphic
+            $feSplat['In'] = 'SourceGraphic'
+        }
+
+        # Create the filter (using [feColorMatrix](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix))
+        $newFilter = SVG.feColorMatrix @feSplat        
+
+        $filterSplat['Content'] =
+            # If the content is a filter, add the filter to the content
+            if ($content.LocalName -eq 'Filter') {
+                @($content.childNodes) + $newFilter
+            } else {
+                $newFilter
+            }
+        
+        # If there's no ID, set it to $defaultId
+        if (-not $filterSplat['ID']) {
+            $filterSplat['ID'] = $defaultId
+        }
+        # Remove parameters from the splat that don't apply to filter.
+        foreach ($notFilterParameter in $notFilterParameters) {
+            $null = $filterSplat.Remove($notFilterParameter)
+        }
+
+        if ($content -and 
+            $content.LocalName -ne 'filter' -and 
+            $content.LocalName -notin $animationElements) {
+            SVG.defs @(SVG.filter @filterSplat)
+            if ($content.setAttribute) {
+                $content.setAttribute('style',"filter:url('#$($filterSplat['ID'])')")
+            }
+            $content
+        } else {
+            SVG.filter @filterSplat
+        }
     }
 }
