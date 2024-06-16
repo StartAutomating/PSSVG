9c780ec786b76bd21862bf27f7cf99b48d6050aa
StartAutomating <StartAutomating@github.com>
2024-06-16 04:51:13 +0000

    feat: SVG.Erode ( Fixes #225 )
        Not passing not filter parameters

--- Commands/Filters/SVG.Erode.ps1
+++ Commands/Filters/SVG.Erode.ps1
@@ -70,6 +70,7 @@ function SVG.Erode {
         begin {
         $animationElements = 'animate','set','animateTransform','animateMotion'
         $defaultId = 'erode'
+        $notFilterParameters = 'Erosion'
     
     }
         process {
@@ -111,7 +112,9 @@ function SVG.Erode {
             $filterSplat['ID'] = $defaultId
         }
         # Remove parameters from the splat that don't apply to filter.
-        $null = $filterSplat.Remove('saturation')
+        foreach ($notFilterParameter in $notFilterParameters) {
+            $null = $filterSplat.Remove($notFilterParameter)
+        }
 
         if ($content -and 
             $content.LocalName -ne 'filter' -and 
